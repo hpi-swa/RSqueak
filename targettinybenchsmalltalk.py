2d0ab2fb3cfb896b7ceeed5c4eb7c0e539714090
Lars Wassermann <lars.wassermann@gmail.com>
2013-03-06 21:10:52 +0100

    (tfel, lwassermann): updated the targettinybenchsmalltalk to reflect the jit test changes needed because of interpreter restructuring

--- targettinybenchsmalltalk.py
+++ targettinybenchsmalltalk.py
@@ -15,45 +15,27 @@ from rpython.jit.codewriter.policy import JitPolicy
 # compile...
 sys.setrecursionlimit(100000)
 
-
-def tinyBenchmarks():
+def setup():
     from spyvm import objspace
     space = objspace.ObjSpace()
     image = create_testimage(space)
-    interp = interpreter.Interpreter(space)
-
+    interp = interpreter.Interpreter(space, image)
+    w_selector = interp.perform(space.wrap_string("loopTest"), "asSymbol")
     w_object = model.W_SmallInteger(0)
-
     s_class = w_object.shadow_of_my_class(space)
-    w_method = s_class.lookup("loopTest")
+    s_method = s_class.lookup(w_selector)
+    s_frame = s_method.create_frame(space, w_object, [])
+    return interp, s_frame
 
-    assert w_method
-    w_frame = w_method.create_frame(space, w_object, [])
-    interp.store_w_active_context(w_frame)
-
-    counter = 0
-
-    from spyvm.interpreter import BYTECODE_TABLE
-    return interp
-
-
-interp = tinyBenchmarks()
-
+interp, s_frame = setup()
 
 def entry_point(argv):
-    counter = 0
     try:
-        while True:
-            counter += 1
-            interp.interpret()
-            if counter == 100000:
-                counter = 0
-                os.write(2, '#')
+        interp.loop(s_frame.w_self())
     except interpreter.ReturnFromTopLevel, e:
         w_result = e.object
-
-    assert isinstance(w_result, model.W_BytesObject)
-    print w_result.as_string()
+        assert isinstance(w_result, model.W_BytesObject)
+        print w_result.as_string()
     return 0
 
 
