3715ff7d83240bd8c800d20adc7d2f7819d9dc3a
Tim Felgentreff <timfelgentreff@gmail.com>
2013-05-03 13:25:27 +0200

    fix benchmark script for stackvm

--- benchmarks.py
+++ benchmarks.py
@@ -202,7 +202,7 @@ Cog = Project(
             callback=(lambda x: subprocess.Popen(["mv", "Squeak-4.10.2.2614-linux_i386", "stackvm"]).wait())
         )
     ],
-    arguments=['-vm-display-X11', '-headless', "images/%s.image" % SqueakImage, '../benchmarks.st'],
+    arguments=['-vm-display-null', "images/%s.image" % SqueakImage, '../benchmarks.st'],
     commitid=cogid
 )
 RSqueakVM = Project(
