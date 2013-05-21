e7b3aacdbc45981f1859bc93239c37e5856ea63f
Lars Wassermann <lars.wassermann@gmail.com>
2013-05-21 11:28:06 +0200

    added printing capabilities to the vm-debugging plugin

--- spyvm/plugins/vmdebugging.py
+++ spyvm/plugins/vmdebugging.py
@@ -27,3 +27,10 @@ def halt(interp, s_frame, w_rcvr):
         print s_frame
         raise Exit('Halt is not well defined when translated.')
     return w_rcvr
+
+@DebuggingPlugin.expose_primitive(unwrap_spec=[object, object])
+def debugPrint(interp, s_frame, w_rcvr, w_string):
+    if not isinstance(w_string, model.W_BytesObject):
+        raise error.PrimitiveFailedError()
+    print w_string.as_string()
+    return w_rcvr
