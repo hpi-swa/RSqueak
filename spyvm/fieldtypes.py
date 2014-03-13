d1dcd8394549029304b8cb1e47bca45b06792b24
Anton <Anton@Anton-Vaio.hpi.uni-potsdam.de>
2014-03-13 19:30:24 +0100

    Replaced obsolete field-types with a FixedSize storage-strategy that simply inherits from ListStorageStrategy.

--- spyvm/fieldtypes.py
+++ spyvm/fieldtypes.py
@@ -292,130 +292,13 @@ class DenseSmallIntegerStorageStrategy(AbstractStorageStrategy):
     def sparse_strategy(self):
         return SparseSmallIntegerStorageStrategy
 
-class TypeTag():
-    pass
 
-LPI = TypeTag()
-SInt = TypeTag()
-flt = TypeTag()
-obj = TypeTag()
-
-# may be used during debugging
-# LPI, SInt, flt, obj = 'LPI', 'SInt', 'float', 'object'
-
-class FieldSort(TimSort):
-    def lt(self, a, b):
-        return a[0] < b[0]
-
-class FixedSizeFieldTypes(AbstractStorageStrategy):
-    _immutable_fields_ = ['types[*]']
-    _attrs_ = ['types', 'parent', 'siblings', 'diff']
-    _settled_ = True
-    
-    erase, unerase = rerased.new_erasing_pair("fixed-size-field-types")
+class FixedSizeStorageStrategy(ListStorageStrategy):
+    erase, unerase = rerased.new_erasing_pair("fixed-size-strategry")
     erase = staticmethod(erase)
     unerase = staticmethod(unerase)
-    strategy_tag = 'fixed'
-    
-    def __init__(self, types, parent=None, change=(-1, obj)):
-        self.types = types
-        self.parent = parent
-        if parent is not None:
-            assert change != (-1, obj)
-        self.diff = change
-        self.siblings = {}
-    
-    def initial_storage(self, space, size):
-        return self.erase([model.w_nil] * size)
-    
-    def storage_for_list(self, space, collection):
-        return self.erase([x for x in collection])
-    
-    def size_of(self, w_obj):
-        return len(self.unerase(w_obj.get_storage()))
-    
-    def fetch(self, space, w_object, n0):
-        w_result = self.unerase(w_object.get_storage())[n0]
-        assert w_result is not None
-        types = self.types
-        # TODO - try 'assert isinstance' instead.
-        if types[n0] is SInt:
-            jit.record_known_class(w_result, model.W_SmallInteger)
-        elif types[n0] is LPI:
-            jit.record_known_class(w_result, model.W_LargePositiveInteger1Word)
-        elif types[n0] is flt:
-            jit.record_known_class(w_result, model.W_Float)
-        return w_result
+    strategy_tag = 'fixed-size'
 
-    def store(self, space, w_object, n0, w_value):
-        types = self.types
-        changed_type = w_value.fieldtype()
-        if types[n0] is not changed_type:
-            w_object.strategy = self.sibling(n0, changed_type)
-        self.unerase(w_object.get_storage())[n0] = w_value
-
-    @jit.elidable
-    def sibling(self, n0, changed_type):
-        assert self.types[n0] is not changed_type
-        change = (n0, changed_type)
-        parent = self.parent
-        siblings = self.siblings
-        if change in siblings:
-            return siblings[change]
-        elif parent is None:
-            return self.descent([change])
-        else:
-            if n0 == self.diff[0]:
-                diff = [change]
-            else:
-                diff = [change, self.diff]
-
-            new_fieldtype = parent.ascent(diff)
-
-            if not objectmodel.we_are_translated():
-                new_types = list(self.types)
-                new_types[n0] = changed_type
-                assert new_fieldtype.types == new_types
-            siblings[change] = new_fieldtype
-            return new_fieldtype
-
-    def ascent(self, changes):
-        parent = self.parent
-        if parent is None:
-            FieldSort(changes).sort()
-            return self.descent(changes)
-        else:
-            change = self.diff
-            if changes[0][0] != change[0]:
-                changes.append(change)
-            return parent.ascent(changes)
-
-    def descent(self, changes):
-        if changes == []:
-            return self
-
-        change = changes[0]
-        if change[1] is obj:
-            return self.descent(changes[1:])
-        siblings = self.siblings
-        if change in siblings:
-            return siblings[change].descent(changes[1:])
-        else:
-            new_types = list(self.types)
-            assert new_types[change[0]] == obj
-            new_types[change[0]] = change[1]
-            new_fieldtype = FixedSizeFieldTypes(new_types, self, change)
-            siblings[change] = new_fieldtype
-            return new_fieldtype.descent(changes[1:])
-
-    @staticmethod
-    @jit.elidable
-    def of_size(n):
-        if n not in maps:
-            maps[n] = FixedSizeFieldTypes([obj] * n)
-        return maps[n]
-
-maps = {}
 
 def strategy_of_size(s_containing_class, size):
     if s_containing_class is None:
@@ -428,10 +311,9 @@ def strategy_of_size(s_containing_class, size):
         # A newly allocated var-sized object contains only nils.
         return AllNilStorageStrategy.singleton
     else:
-        # TODO -- use strategy-concept for fixed-size classes also.
         # TODO -- monitor classes and create heuristic-based default strategies 
         # that should be more suited. Example: 2 small-integers for Point.
-        return FixedSizeFieldTypes.of_size(size)
+        return FixedSizeStorageStrategy.singleton
 
 def strategy_for_list(s_containing_class, vars):
     if s_containing_class is None:
@@ -466,11 +348,5 @@ def strategy_for_list(s_containing_class, vars):
         else:
             return SparseSmallIntegerStorageStrategy.singleton
     else:
-        size = len(vars)
-        typer = FixedSizeFieldTypes.of_size(size)
-        for i, w_val in enumerate(vars):
-            changed_type = w_val.fieldtype()
-            if changed_type is not obj:
-                typer = typer.sibling(i, changed_type)
-        return typer
+        FixedSizeStorageStrategy.singleton
     
