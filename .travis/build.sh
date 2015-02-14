49da32ab055eb00227790d91aeec2b871a30844c
Tim Felgentreff <timfelgentreff@gmail.com>
2015-02-14 12:31:27 +0100

    upload the master binary

--- .travis/build.sh
+++ .travis/build.sh
@@ -7,3 +7,13 @@ sudo i386 chroot "$chroot" sh -c "
     ls &&
     PYTHONPATH=\"$PYTHONPATH:pypy-pypy/:pypy-rsdl/:.\"\
             python2.7 pypy-pypy/rpython/bin/rpython --batch -Ojit targetimageloadingsmalltalk.py"
+
+exitcode=$?
+if [ $exitcode -eq 0 ]; then
+    if [ "$TRAVIS_BRANCH" == "master" ]; then
+	if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+	    mv rsqueak rsqueak-x86-Linux-jit-\"$TRAVIS_COMMIT\"
+	    curl -T rsqueak-x86-Linux-jit-* http://www.lively-kernel.org/babelsberg/RSqueak/
+	fi
+    fi
+fi
