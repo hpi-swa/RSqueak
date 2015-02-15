bac288fce7c787b01a0febd6d7a17d4f35ef98b7
Tim Felgentreff <timfelgentreff@gmail.com>
2015-02-15 09:25:48 +0100

    unquote

--- .travis/build.sh
+++ .travis/build.sh
@@ -12,7 +12,7 @@ exitcode=$?
 if [ $exitcode -eq 0 ]; then
     if [ "$TRAVIS_BRANCH" == "master" ]; then
 	if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-	    mv rsqueak rsqueak-x86-Linux-jit-\"$TRAVIS_COMMIT\"
+	    mv rsqueak rsqueak-x86-Linux-jit-$TRAVIS_COMMIT
 	    curl -T rsqueak-x86-Linux-jit-* http://www.lively-kernel.org/babelsberg/RSqueak/
 	fi
     fi
