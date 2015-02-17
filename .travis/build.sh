53769f7cdfc8e175b0017da59b211231b584271d
Tim Felgentreff <timfelgentreff@gmail.com>
2015-02-17 22:33:25 +0100

    Update build.sh

--- .travis/build.sh
+++ .travis/build.sh
@@ -8,17 +8,24 @@ if [ "$TEST_TYPE" == "build" ]; then
     ls &&
     PYTHONPATH=\"$PYTHONPATH:pypy-pypy/:pypy-rsdl/:.\"\
             python2.7 pypy-pypy/rpython/bin/rpython --batch -Ojit targetimageloadingsmalltalk.py"
-
+    mv rsqueak* rsqueak-x86-Linux-jit-$TRAVIS_COMMIT || true
     exitcode=$?
-    if [ $exitcode -eq 0 ]; then
-        if [ "$TRAVIS_BRANCH" == "master" ]; then
-	    if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-	        mv rsqueak rsqueak-x86-Linux-jit-$TRAVIS_COMMIT
-                curl -T rsqueak-x86-Linux-jit-* http://www.lively-kernel.org/babelsberg/RSqueak/
-	    fi
-        fi
-    fi
-    exit $exitcode
+else if [ "$TEST_TYPE" == "64bitbuild" ]; then
+    echo $(pwd)
+    ls
+    PYTHONPATH="$PYTHONPATH:pypy-pypy/:pypy-rsdl/:." python2.7 \
+            pypy-pypy/rpython/bin/rpython --batch -Ojit targetimageloadingsmalltalk.py
+    mv rsqueak* rsqueak-x86_64-Linux-jit-$TRAVIS_COMMIT || true
+    exitcode=$?
 else
     exit 0
+fi fi
+
+if [ $exitcode -eq 0 ]; then
+    if [ "$TRAVIS_BRANCH" == "master" ]; then
+        if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+            curl -T rsqueak-x86* http://www.lively-kernel.org/babelsberg/RSqueak/
+	fi
+    fi
 fi
+exit $exitcode
