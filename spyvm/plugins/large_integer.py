b16075add1da65d34578f03083b8e793cdaa7732
Tim Felgentreff <timfelgentreff@gmail.com>
2015-02-27 18:51:57 +0100

    attempt to use the 32bit bit operations for large integers

--- spyvm/plugins/large_integer.py
+++ spyvm/plugins/large_integer.py
@@ -1,4 +1,5 @@
 from spyvm import model
+from spyvm.primitives import BIT_AND, BIT_OR, BIT_XOR, prim_table
 from spyvm.error import PrimitiveFailedError
 from spyvm.plugins.plugin import Plugin
 
@@ -9,22 +10,40 @@ LargeIntegerPlugin = Plugin()
 
 
 ops = {
-    'primDigitBitAnd': (rbigint.and_, False),
-    'primDigitBitOr': (rbigint.or_, False),
-    'primDigitBitXor': (rbigint.xor, False),
-    'primDigitAdd': (rbigint.add, True),
-    'primDigitSubtract': (rbigint.sub, True)
+    'primDigitBitAnd': (rbigint.and_, BIT_AND),
+    'primDigitBitOr': (rbigint.or_, BIT_OR),
+    'primDigitBitXor': (rbigint.xor, BIT_XOR)
 }
-for (name, (op, absolute)) in ops.items():
-    def make_func(op, absolute):
-        def func(interp, s_frame, a, b):
-            if absolute:
-                a = a.abs()
-                b = b.abs()
+for (name, (op, primitive)) in ops.items():
+    def make_func(op, primitive):
+        primfunc = prim_table[primitive]
+        def bigfunc(interp, s_frame, a, b):
             return interp.space.wrap_bigint(op(a, b))
+        bigfunc.func_name = "some_unlikely_%s" % name
+        LargeIntegerPlugin.expose_primitive(unwrap_spec=[rbigint, rbigint])(bigfunc)
+        wrapped_bigfunc = LargeIntegerPlugin.prims[bigfunc.func_name]
+        # We try to run the 32bit integer primitive first
+        def func(interp, s_frame, argcount):
+            try:
+                primfunc(interp, s_frame, argcount)
+            except PrimitiveFailedError:
+                wrapped_bigfunc(interp, s_frame, argcount)
+        func.func_name = name
+        LargeIntegerPlugin.expose_primitive(clean_stack=False, no_result=True)(func)
+    make_func(op, primitive)
+
+
+arithOps = {
+    'primDigitAdd': rbigint.add,
+    'primDigitSubtract': rbigint.sub
+}
+for (name, op) in arithOps.items():
+    def make_func(op):
+        def func(interp, s_frame, a, b):
+            return interp.space.wrap_bigint(op(a.abs(), b.abs()))
         func.func_name = name
         LargeIntegerPlugin.expose_primitive(unwrap_spec=[rbigint, rbigint])(func)
-    make_func(op, absolute)
+    make_func(op)
 
 
 @LargeIntegerPlugin.expose_primitive(unwrap_spec=[rbigint, rbigint, bool])
