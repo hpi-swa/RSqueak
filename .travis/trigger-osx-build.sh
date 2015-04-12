c47fa5efed1a808a2ab93a4d3977641de32ba078
Tim Felgentreff <timfelgentreff@gmail.com>
2015-04-12 17:31:49 +0200

    simplify

--- .travis/trigger-osx-build.sh
+++ .travis/trigger-osx-build.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 set -ex
 
-function doIt {
-    gem install travis --no-ri --no-rdoc
-    travis login --no-interactive -g $GH_TOKEN
-    travis restart --no-interactive -r timfel/RSqueak-MacOSXBuild
-}
-
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     if [ "$BUILD_TYPE" == "build" ]; then
 	if [ "$BUILD_ARCH" == "32bit" ]; then
 	    if [ "$TRAVIS_BRANCH" == "master" ]; then
-		doIt
-	    fi
-	    if [ "$TRAVIS_BRANCH" == "osx-build" ]; then
-		doIt
+		gem install travis --no-ri --no-rdoc
+		travis login --no-interactive -g $GH_TOKEN
+		travis restart --no-interactive -r timfel/RSqueak-MacOSXBuild
 	    fi
 	fi
     fi
