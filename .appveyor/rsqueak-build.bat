7fe99beacfcb10d970364b8d4ded1d49a9a8e443
Tim Felgentreff <timfelgentreff@gmail.com>
2015-04-12 16:36:04 +0200

    name differently optimized binaries differently

--- .appveyor/rsqueak-build.bat
+++ .appveyor/rsqueak-build.bat
@@ -4,4 +4,4 @@ set PYTHONPATH=%~dp0\;%~dp0\pypy;%~dp0\rsdl
 echo %SDL_PREFIX%
 echo %PYTHONPATH%
 %~dp0\pypy-win32\pypy.exe %~dp0\pypy\rpython\bin\rpython --batch %OPTLVL% %~dp0\targetrsqueak.py
-copy %~dp0\rsqueak.exe %~dp0\rsqueak-win32-%APPVEYOR_REPO_COMMIT%.exe
+copy %~dp0\rsqueak.exe %~dp0\rsqueak-win32%OPTLVL%-%APPVEYOR_REPO_COMMIT%.exe
