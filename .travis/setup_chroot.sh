f07f8d8491349881153ad470e2bf6bf1b75bdd4c
Tim Felgentreff <timfelgentreff@gmail.com>
2015-04-13 12:00:52 +0200

    cache chroot

--- .travis/setup_chroot.sh
+++ .travis/setup_chroot.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -ex
 
-# Prepare an i386 chroot. This is required as we otherwise can't install
-# our dependencies to be able to compile a 32bit binary. Ubuntu...
-mkdir -p "$chroot$PWD"
 sudo apt-get install -y debootstrap
-sudo i386 debootstrap --arch=i386 precise "$chroot"
-sudo mount --rbind "$PWD" "$chroot$PWD"
-sudo i386 chroot "$chroot" apt-get install -y build-essential
+if [ ! -d "$chroot" ]; then
+    # Prepare an i386 chroot. This is required as we otherwise can't install
+    # our dependencies to be able to compile a 32bit binary. Ubuntu...
+    mkdir -p "$chroot$PWD"
+    sudo i386 debootstrap --arch=i386 precise "$chroot"
+    sudo i386 chroot "$chroot" apt-get install -y build-essential
+    # Now install our dependencies.
+    sudo i386 chroot "$chroot" apt-get install -y libsdl1.2-dev libffi-dev python2.7
+fi
 
-# Now install our dependencies.
-sudo i386 chroot "$chroot" apt-get install -y libsdl1.2-dev libffi-dev python2.7
+sudo mount --rbind "$PWD" "$chroot$PWD"
