398eed79d011ac37d7d3f65cf05ccf8238743938
Tim Felgentreff <timfelgentreff@gmail.com>
2015-04-24 17:28:29 +0200

    Update rsqueak-build.bat

--- .appveyor/rsqueak-build.bat
+++ .appveyor/rsqueak-build.bat
@@ -1,3 +1,3 @@
 C:\Python27\python %~dp0\..\.build\download_dependencies.py
 %~dp0\..\.build\pypy-win32\pypy.exe %~dp0\..\.build\build.py
-copy %~dp0\rsqueak.exe %~dp0\rsqueak-win32%OPTLVL%-%APPVEYOR_REPO_COMMIT%.exe
+copy %~dp0\..\rsqueak.exe %~dp0\..\rsqueak-win32%OPTLVL%-%APPVEYOR_REPO_COMMIT%.exe
