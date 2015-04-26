4fb5bd12cd6a54426b93e45dc37930c44a75d3be
Tim Felgentreff <timfelgentreff@gmail.com>
2015-04-26 17:07:38 +0200

    be more forgiving when running 64-bit mode, and switch automatically, printing some platform specific information about 32-bit prerequisites

--- .build/build.py
+++ .build/build.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     if not reduce(bool.__or__, [arg.startswith("-O") for arg in sys.argv]):
         sys.argv.append("--batch")
         sys.argv.append("-Ojit")
+    sys.argv.append("--gcrootfinder=shadowstack") # default to shadowstack, so all platforms are created equal
     sys.argv.append(os.path.join(os.path.dirname(__file__), "..", "targetrsqueak.py"))
     import environment
     from rpython.translator.goal.translate import main
