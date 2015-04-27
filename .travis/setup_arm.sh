96b55295a3cbd2f2e2451b54800b509888edfa0a
Tim Felgentreff <timfelgentreff@gmail.com>
2015-04-27 23:50:44 +0200

    see if we can build for raspbian

--- .travis/setup_arm.sh
+++ .travis/setup_arm.sh
@@ -2,12 +2,12 @@ set -ex
 sudo apt-get update
 
 # install arm environment
-sudo apt-get install -y debootstrap schroot binfmt-support qemu-system qemu-user-static scratchbox2 gcc-arm-linux-gnueabi libsdl1.2-dev libffi-dev
-mkdir precise_arm
+sudo apt-get install -y debootstrap schroot binfmt-support qemu-system qemu-user-static scratchbox2 gcc-arm-linux-gnueabihf libsdl1.2-dev libffi-dev
+mkdir raspbian_arm
 sudo chown $USER /etc/schroot/schroot.conf
 echo "
-[precise_arm]
-directory=$PWD/precise_arm
+[raspbian_arm]
+directory=$PWD/raspbian_arm
 users=$USER
 root-users=$USER
 groups=$USER
@@ -17,11 +17,15 @@ type=directory
 cat /etc/schroot/schroot.conf
 sudo chown root /etc/schroot/schroot.conf
 
-sudo qemu-debootstrap --variant=buildd --arch=armel precise precise_arm/ http://ports.ubuntu.com/ubuntu-ports/
-schroot -c precise_arm -- uname -m
-sudo su -c 'echo "deb http://ports.ubuntu.com/ubuntu-ports/ precise main universe restricted" > precise_arm/etc/apt/sources.list'
-schroot -c precise_arm -u root -- apt-get update
-schroot -c precise_arm -u root -- apt-get install -y libffi-dev python-dev build-essential libsdl1.2-dev
-cd precise_arm/
-sb2-init -c `which qemu-arm` ARM `which arm-linux-gnueabi-gcc`
+wget http://archive.raspbian.org/raspbian.public.key
+sudo apt-key add raspbian.public.key
+
+sudo qemu-debootstrap --variant=buildd --keyring /etc/apt/trusted.gpg --arch armhf --foreign wheezy\
+   raspbian_arm/ http://archive.raspbian.org/raspbian/
+schroot -c raspbian_arm -- uname -m
+sudo su -c 'echo "deb http://archive.raspbian.org/raspbian/ wheezy main universe restricted" > raspbian_arm/etc/apt/sources.list'
+schroot -c raspbian_arm -u root -- apt-get update
+schroot -c raspbian_arm -u root -- apt-get install -y libffi-dev build-essential libsdl1.2-dev # python-dev 
+cd raspbian_arm/
+sb2-init -c `which qemu-arm` ARM `which arm-linux-gnueabihf-gcc`
 cd ..
