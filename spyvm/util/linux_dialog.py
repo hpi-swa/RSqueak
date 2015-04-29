021e4c27d4ea5c0dc6d5b86ecd582b662eddb95c
Tim Felgentreff <timfelgentreff@gmail.com>
2015-04-29 12:48:01 +0200

    use fltk only as fallback

--- spyvm/util/linux_dialog.py
+++ spyvm/util/linux_dialog.py
@@ -3,7 +3,7 @@ from rpython.translator.platform import CompilationError
 from rpython.rtyper.lltypesystem import lltype, rffi
 from rpython.rtyper.tool import rffi_platform as platform
 import py
-import os, sys
+import os, sys, subprocess
 from spyvm.util import system
 
 assert system.IS_LINUX
@@ -12,7 +12,8 @@ this_dir = py.path.local(__file__).dirpath()
 
 ###############################################################################
 
-import subprocess
+_Default = "Squeak.image"
+
 if subprocess.call("fltk-config --version", shell=True) == 0:
     cflags = subprocess.check_output("fltk-config --cxxflags", shell=True).strip().split()
     ldflags = subprocess.check_output("fltk-config --ldstaticflags", shell=True).strip().split()
@@ -31,7 +32,7 @@ if subprocess.call("fltk-config --version", shell=True) == 0:
     )
 
     __llget_file = rffi.llexternal('RSqueakOpenFileDialog_linux', [rffi.CCHARP, rffi.INT], rffi.INT, compilation_info=eci)
-    def get_file():
+    def fltk_get_file():
         charp = rffi.str2charp("".join(["\0"] * 260))
         res = __llget_file(charp, 260)
         if (res == 1):
@@ -39,27 +40,30 @@ if subprocess.call("fltk-config --version", shell=True) == 0:
             rffi.free_charp(charp)
             return path
         else:
-            return "Squeak.image"
+            return _Default
 else:
-    print "\033[33;5;7m You do not have FLTK installed - file picker will fall back to Zenity at runtime \033[0m"
-    def get_file():
-        default = "Squeak.image"
-        r, w = os.pipe()
-        pid = os.fork()
-        if pid == 0:
-            os.close(r)
-            os.dup2(w, 1)
-            os.execv("/bin/sh", ["/bin/sh", "-c", "zenity2 --file-selection --file-filter='*.image'"])
+    print "\033[33;5;7m You do not have FLTK installed - file picker will have no fall back if Zenity is not available at runtime \033[0m"
+    def fltk_get_file():
+        print "\033[33;5;7m You do not have Zenity installed - file picker not available \033[0m"
+        return _Default
+
+def get_file():
+    r, w = os.pipe()
+    pid = os.fork()
+    if pid == 0:
+        os.close(r)
+        os.dup2(w, 1)
+        os.execv("/bin/sh", ["/bin/sh", "-c", "zenity --file-selection --file-filter='*.image'"])
+    else:
+        os.close(w)
+        try:
+            pid, status = os.waitpid(pid, 0)
+        except OSError as e:
+            return _Default
+        status = os.WEXITSTATUS(status)
+        if status == 127:
+            return fltk_get_file()
+        elif status == 0:
+            return os.read(r, 512).replace("\n", "")
         else:
-            os.close(w)
-            try:
-                pid, status = os.waitpid(pid, 0)
-            except OSError as e:
-                return default
-            status = os.WEXITSTATUS(status)
-            if status != 0:
-                if status == 127:
-                    print "\033[33;5;7m You do not have Zenity installed - file picker not working \033[0m"
-                return default
-            else:
-                return os.read(r, 512).replace("\n", "")
+            return _Default
