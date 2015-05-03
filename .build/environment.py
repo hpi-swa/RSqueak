f1a70ac5dfb55968bfc38c111d051be39e4af27c
Jakob Reschke <jakob.reschke@student.hpi.de>
2015-05-03 16:54:04 +0200

    set build environment variables before rpython is imported
    because the mere importing of rsqueak sometimes needs cl.exe on Windows

--- .build/environment.py
+++ .build/environment.py
@@ -62,12 +62,6 @@ def ensure_32bit_environment():
 
 
 def prepare_environment_variables():
-    sys.path.insert(0, cp.get("General", "pypy"))
-    sys.path.insert(0, cp.get("General", "rsdl"))
-    try:
-        import targetrsqueak as rsqueak
-    except ImportError:
-        sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
     if "nt" == os.name:
         vs = cp.get("Windows", "VisualStudio9")
         sdk = cp.get("Windows", "WindowsSDK7")
@@ -87,6 +81,12 @@ def prepare_environment_variables():
         pass
     else:
         raise AssertionError("Unsupported platform")
+    sys.path.insert(0, cp.get("General", "pypy"))
+    sys.path.insert(0, cp.get("General", "rsdl"))
+    try:
+        import targetrsqueak as rsqueak
+    except ImportError:
+        sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 
 
 cp, config = load_config()
