d559bf410987205642332657cb065cdcf2f04f31
Bastian Kruck <bastian.kruck@student.hpi.de>
2015-05-15 16:21:17 +0200

    fixed pypy-error and documented simulation plugin

--- spyvm/plugins/simulation.py
+++ spyvm/plugins/simulation.py
@@ -6,6 +6,21 @@ from spyvm.plugins.plugin import Plugin
 from rpython.rlib import jit, objectmodel
 from rpython.rlib.rarithmetic import r_uint, intmask
 
+# If an EXTERNAL_CALL for the given moduleName and functionName is not found,
+# the SimulationPlugin is used to enable the image simulating that primitive.
+# For that, the simulatePrimitive: aFunctionName args: aCollection message is sent
+# to the receiver of the EXTERNAL_CALL primitive.
+# Note that the moduleName parameter of EXTERNAL_CALL is omitted and has no influence on the simulation!
+
+# simulatePrimitive:args: may return a value to pass it to the primitive caller and
+# should raise primitive 255 META_PRIM_FAILED (e.g. via InterpreterProxy>>primitiveFailFor:)
+# if the primitive failed and the fallback code should be run.
+
+# Example:
+# BitBlt>>simulatePrimitive: aFunctionName args: aCollection
+#     aFunctionName = 'primitiveCopyBits' ifTrue: [ ^self copyBitsSimulated ].
+#     aFunctionName = 'primitiveWarpBits' ifTrue: [ ^self warpBitsSimulated: (args at: 1) sourceMap: (args at: 2) ].
+#     ^InterpreterProxy new primitiveFailFor: 255.
 
 class SimulationPluginClass(Plugin):
     def simulate(self, w_name, name, interp, s_frame, argcount, w_method):
@@ -37,10 +52,10 @@ class SimulationPluginClass(Plugin):
             # must clean the stack, including the rcvr
             s_frame.pop_n(argcount + 1)
             s_frame.push(ret.value)
-            return True
+            return w_rcvr
         except MetaPrimFailed, e:
             interp.stack_frame(s_fallback, None)
-            return False
+            return w_rcvr
         else:
             raise PrimitiveFailedError
 
