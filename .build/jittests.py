e76315bb9dff5a5c49394559ead468694ec182da
Jakob Reschke <jakob.reschke@student.hpi.de>
2015-09-26 21:36:46 +0200

    replace more reduce(bool.__or__, ...) by any(...)

--- .build/jittests.py
+++ .build/jittests.py
@@ -1,18 +1,18 @@
 #! /usr/bin/env python
 
-import sys, zlib, base64
+import sys
 from os import path
 from environment import cp, config
 
 if __name__ == "__main__":
-    if not reduce(bool.__or__, [arg.startswith("--jit") for arg in sys.argv]):
+    if not any(arg.startswith("--jit") for arg in sys.argv):
         sys.argv.append("--jit=%s" % path.join(path.dirname(__file__), "..", "rsqueak"))
-    if not reduce(bool.__or__, [arg.startswith("--squeak") for arg in sys.argv]):
+    if not any(arg.startswith("--squeak") for arg in sys.argv):
         squeak = cp.get("General", "squeak")
         if not path.exists(squeak):
             print "WARN: You need to configure where to find the squeak binary in %s to run the full suite of JIT tests" % config
         sys.argv.append("--squeak=%s" % squeak)
-    if not reduce(bool.__or__, [arg.find("jittest/test") != -1 for arg in sys.argv]):
+    if not any(arg.find("jittest/test") != -1 for arg in sys.argv):
         sys.argv.append(path.join(path.dirname(__file__), "..", "spyvm", "test", "jittest"))
 
     import pytest
