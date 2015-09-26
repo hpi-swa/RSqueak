d8e9d7cd614d630f8d6ff6367b720c227edf96ae
Jakob Reschke <jakob.reschke@student.hpi.de>
2015-09-26 17:34:37 +0200

    supply PYTHONPATH to the terminal started from .build/shell.py

--- .build/shell.py
+++ .build/shell.py
@@ -6,5 +6,11 @@ if __name__ == "__main__":
     import environment, code
     print "\033[33;5;7m \nUse terminal() to drop into a system console\n \033[0m"
     def terminal():
+        module_path = os.path.abspath(os.path.dirname(__file__))
+        if module_path in sys.path:
+            paths = sys.path[:sys.path.index(module_path)]
+        else:
+            paths = sys.path
+        os.putenv("PYTHONPATH", os.pathsep.join(paths))
         os.system(os.environ.get("COMSPEC") or os.environ.get("SHELL") or "/bin/sh")
     code.interact(local=locals())
