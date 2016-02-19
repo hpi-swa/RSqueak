e743e02ab4aab5d37921752b699e6fd665019031
Tim Felgentreff <timfelgentreff@gmail.com>
2016-02-19 18:19:30 +0100

    Revert "run unittests with dummy videodriver"
    This reverts commit ccac56a203db46796037f716bb5331914b52ce69.

--- .build/unittests.py
+++ .build/unittests.py
@@ -1,11 +1,14 @@
 #! /usr/bin/env python
 
-import sys, os
+#! /usr/bin/env python
+
+import sys
+from os import path
 import environment
 
 if __name__ == "__main__":
     if not any("/test" in arg or "\\test" in arg for arg in sys.argv):
-        sys.argv.append(os.path.join(os.path.dirname(__file__), "..", "spyvm", "test"))
-    os.environ["SDL_VIDEODRIVER"] = "dummy"
+        sys.argv.append(path.join(path.dirname(__file__), "..", "spyvm", "test"))
+
     import pytest
     exit(pytest.main(args=sys.argv))
