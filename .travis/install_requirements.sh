9fda017df3169ad9e45ef7b7db51ebb0cbfb83cb
Tim Felgentreff <timfelgentreff@gmail.com>
2016-02-19 10:41:39 +0100

    setup SDL2 locally on 64-bit Linux

--- .travis/install_requirements.sh
+++ .travis/install_requirements.sh
@@ -12,30 +12,11 @@ setup_osx() {
 }
 
 setup_linux() {
-    # borrowed this from https://github.com/omf2097/openomf/blob/master/.travis.yml
-    # libsdl2-dev is not available yet on Ubuntu 12.04, which is employed by Travis
-    if [ ! -d "$HOME/SDL2/lib" ]; then
-        wget https://www.libsdl.org/release/SDL2-2.0.3.tar.gz -O ~/SDL2.tar.gz
-        tar -xzvf ~/SDL2.tar.gz -C ~/
-        mkdir ~/sdl-build
-        pushd ~/sdl-build
-        CONFIG_FLAGS=
-        if [ "$BUILD_ARCH" == "32bit" -o "$BUILD_ARCH" == "lldebug" ]; then
-            CONFIG_FLAGS="--build=i686-pc-linux-gnu \
-                CFLAGS=-m32 CXXFLAGS=-m32 LDFLAGS=-m32"
-        fi
-        ~/SDL2-2.0.3/configure --prefix=$HOME/SDL2 $CONFIG_FLAGS
-        make
-        make install
-        popd
-    else
-        echo 'Using cached SDL2 build directory.'
-    fi
+    # on Linux, libsdl2 is installed through our dependencies stuff
     wget http://squeakvm.org/unix/release/Squeak-4.10.2.2614-linux_i386.tar.gz
     tar xzvf Squeak-4.10*.tar.gz
     rm Squeak-4.10*.tar.gz
     ln -s $PWD/Squeak-4.10*/bin/squeak .build/squeak
-
 }
 
 setup_$TRAVIS_OS_NAME
