ccac56a203db46796037f716bb5331914b52ce69
Tim Felgentreff <timfelgentreff@gmail.com>
2016-02-19 16:01:02 +0100

    run unittests with dummy videodriver

--- .build/unittests.py
+++ .build/unittests.py
@@ -1,14 +1,11 @@
 #! /usr/bin/env python
 
-#! /usr/bin/env python
-
-import sys
-from os import path
+import sys, os
 import environment
 
 if __name__ == "__main__":
     if not any("/test" in arg or "\\test" in arg for arg in sys.argv):
-        sys.argv.append(path.join(path.dirname(__file__), "..", "spyvm", "test"))
-
+        sys.argv.append(os.path.join(os.path.dirname(__file__), "..", "spyvm", "test"))
+    os.environ["SDL_VIDEODRIVER"] = "dummy"
     import pytest
     exit(pytest.main(args=sys.argv))
