85286ab9de32d2fdf653369b8f78491d8bd8f9d6
Tobias Pape <tobias@netshed.de>
2016-02-24 23:54:22 +0100

    Let digitAdd/Subtract work on some positive numbers
    else, fall back. We have to do it this way, as the implementation
    of ADD/SUB is differently unwrapped

--- spyvm/plugins/large_integer.py
+++ spyvm/plugins/large_integer.py
@@ -1,22 +1,23 @@
 from spyvm import model
 from spyvm.primitives import prim_table, \
-    BIT_AND, BIT_OR, BIT_XOR, BIT_SHIFT, ADD, SUBTRACT, DIVIDE, MULTIPLY
+    BIT_AND, BIT_OR, BIT_XOR, BIT_SHIFT, ADD, SUBTRACT, DIVIDE, MULTIPLY, \
+    pos_32bit_int
 from spyvm.error import PrimitiveFailedError
 from spyvm.plugins.plugin import Plugin
 
+from rpython.rlib.rarithmetic import r_longlong, intmask, r_uint, ovfcheck
+import operator
 
 LargeIntegerPlugin = Plugin()
 
 
-ops = {
+bitops = {
     'primDigitBitAnd': BIT_AND,
     'primDigitBitOr': BIT_OR,
     'primDigitBitXor': BIT_XOR,
     'primDigitBitShiftMagnitude': BIT_SHIFT,
-    'primDigitAdd': ADD,
-    'primDigitSubtract': SUBTRACT,
 }
-for (name, primitive) in ops.items():
+for name, primitive in bitops.items():
     def make_func(primitive):
         primfunc = prim_table[primitive]
         def func(interp, s_frame, argcount):
@@ -25,12 +26,11 @@ for (name, primitive) in ops.items():
         LargeIntegerPlugin.expose_primitive(clean_stack=False, no_result=True)(func)
     make_func(primitive)
 
-
 negOps = {
     'primDigitDivNegative': DIVIDE,
     'primDigitMultiplyNegative': MULTIPLY
 }
-for (name, primitive) in negOps.items():
+for name, primitive in negOps.items():
     def make_func(primitive):
         primfunc = prim_table[primitive]
         def func(interp, s_frame, argcount):
@@ -41,3 +41,26 @@ for (name, primitive) in negOps.items():
         func.func_name = name
         LargeIntegerPlugin.expose_primitive(clean_stack=False, no_result=True)(func)
     make_func(primitive)
+
+# cannot reuse base impl here, different unwrap
+ops = {
+    'primDigitAdd': operator.add,
+    'primDigitSubtract': operator.sub,
+}
+for name, op in ops.items():
+    def make_func(op):
+        def func(interp, s_frame, receiver, argument):
+            try:
+                if isinstance(receiver, r_uint) and isinstance(argument, r_uint):
+                    res = op(intmask(receiver), intmask(argument))
+                    return interp.space.wrap_positive_32bit_int(intmask(res))
+                elif isinstance(receiver, int) and isinstance(argument, int):
+                    res = ovfcheck(op(intmask(receiver), intmask(argument)))
+                    return interp.space.wrap_int(intmask(res))
+                else:
+                    raise PrimitiveFailedError()
+            except OverflowError:
+                raise PrimitiveFailedError()
+        func.func_name = name
+        LargeIntegerPlugin.expose_primitive(unwrap_specs=[[int, int], [pos_32bit_int, pos_32bit_int], [r_uint, r_uint]])(func)
+    make_func(op)
