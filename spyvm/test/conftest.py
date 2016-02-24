72ac32858b23f71c178dbca9ee99feb7a066d097
Tobias Pape <tobias@netshed.de>
2016-02-24 11:01:25 +0100

    discriminate when to use headless in jittests

--- spyvm/test/conftest.py
+++ spyvm/test/conftest.py
@@ -63,7 +63,12 @@ def pytest_funcarg__spy(request):
         return _Executor_(py.path.local(val), jitarg)
 
 def pytest_funcarg__squeak(request):
+    import sys
     val = request.config.getvalue("squeakbinary")
     if not val:
         py.test.skip("Provide --squeak parameter to execute modern jit tests")
-    return _Executor_(py.path.local(val), "-nodisplay")
+    if sys.platform.startswith('linux'):
+        # -headless is not sufficient on linux
+        return _Executor_(py.path.local(val), "-nodisplay")
+    else:
+        return _Executor_(py.path.local(val), "-headless")
