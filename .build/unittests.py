42624b5cb8c522c244e836f3e46acc770d564974
Tim Felgentreff <timfelgentreff@gmail.com>
2016-02-24 11:37:50 +0100

    hack around python segfaults by fixing the order of some test modules

--- .build/unittests.py
+++ .build/unittests.py
@@ -3,18 +3,34 @@
 #! /usr/bin/env python
 
 import sys
-from os import path
+from os import path, walk
 import environment
 
-# These should run first, because they call allInstances and become, which is
+# These should run alone, because they call allInstances and become, which is
 # hard on memory and can break on some PyPy releases if run late in the game
-SHOULD_RUN_FIRST = ["test_miniimage_compiling.py", "test_primitives.py"]
+SHOULD_RUN_FIRST = [
+    "test_miniimage.py",
+    "test_primitives.py",
+    "test_miniimage_compiling.py"
+]
 
 if __name__ == "__main__":
     import pytest
     if not any("/test" in arg or "\\test" in arg for arg in sys.argv):
         testdir = path.join(path.dirname(__file__), "..", "spyvm", "test")
-        for n in SHOULD_RUN_FIRST: sys.argv.append(path.join(testdir, n))
-        sys.argv.append(testdir)
-
-    exit(pytest.main(args=sys.argv))
+        exitcode = 0
+        args = sys.argv[:]
+        for n in SHOULD_RUN_FIRST:
+            args.append(path.join(testdir, n))
+            exitcode |= pytest.main(args=args)
+            args.pop()
+        args = sys.argv[:]
+        for root, subdirs, files in walk(testdir):
+            files.sort()
+            for f in files:
+                if f.startswith("test_") and f.endswith(".py") and (not f in SHOULD_RUN_FIRST):
+                    args.append(path.join(root, f))
+        exitcode |= pytest.main(args=args)
+        exit(exitcode)
+    else:
+        exit(pytest.main(args=sys.argv))
