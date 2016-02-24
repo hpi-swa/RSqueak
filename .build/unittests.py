d34c4753c7894a3e6dca52c50bc93cec5c3756f9
Tim Felgentreff <timfelgentreff@gmail.com>
2016-02-24 11:15:41 +0100

    run some tests before others so they go faster / use less memory

--- .build/unittests.py
+++ .build/unittests.py
@@ -6,9 +6,15 @@ import sys
 from os import path
 import environment
 
-if __name__ == "__main__":
-    if not any("/test" in arg or "\\test" in arg for arg in sys.argv):
-        sys.argv.append(path.join(path.dirname(__file__), "..", "spyvm", "test"))
+# These should run first, because they call allInstances and become, which is
+# hard on memory and can break on some PyPy releases if run late in the game
+SHOULD_RUN_FIRST = ["test_miniimage_compiling.py", "test_primitives.py"]
 
+if __name__ == "__main__":
     import pytest
+    if not any("/test" in arg or "\\test" in arg for arg in sys.argv):
+        testdir = path.join(path.dirname(__file__), "..", "spyvm", "test")
+        for n in SHOULD_RUN_FIRST: sys.argv.append(path.join(testdir, n))
+        sys.argv.append(testdir)
+
     exit(pytest.main(args=sys.argv))
