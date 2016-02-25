3a8e5c2a0ec994ebdf9f4dcbf4981360f352b380
Tobias Pape <tobias@netshed.de>
2016-02-25 09:34:29 +0100

    Simplify digitAdd/Subtract logic

--- spyvm/plugins/large_integer.py
+++ spyvm/plugins/large_integer.py
@@ -39,28 +39,26 @@ for name, primitive in negOps.items():
             neg = interp.space.w_true is s_frame.pop()
             return primfunc(interp, s_frame, 2)
         func.func_name = name
-        LargeIntegerPlugin.expose_primitive(clean_stack=False, no_result=True)(func)
+        LargeIntegerPlugin.expose_primitive()(func)
     make_func(primitive)
 
-# cannot reuse base impl here, different unwrap
 ops = {
-    'primDigitAdd': operator.add,
-    'primDigitSubtract': operator.sub,
+    'primDigitAdd': ADD,
+    'primDigitSubtract': SUBTRACT,
 }
-for name, op in ops.items():
-    def make_func(op):
-        def func(interp, s_frame, receiver, argument):
-            try:
-                if isinstance(receiver, r_uint) and isinstance(argument, r_uint):
-                    res = op(intmask(receiver), intmask(argument))
-                    return interp.space.wrap_positive_32bit_int(intmask(res))
-                elif isinstance(receiver, int) and isinstance(argument, int):
-                    res = ovfcheck(op(intmask(receiver), intmask(argument)))
-                    return interp.space.wrap_int(intmask(res))
-                else:
-                    raise PrimitiveFailedError()
-            except OverflowError:
-                raise PrimitiveFailedError()
+for name, primitive in ops.items():
+    def make_func(primitive):
+        primfunc = prim_table[primitive]
+        def func(interp, s_frame, argcount):
+            """This operation is only supported for sign-matching
+            receiver/argument pairs."""
+            if 2 < argcount or argcount < 1:
+                raise PrimitiveFailedError
+            rcvr = s_frame.peek(argcount).unwrap_longlong(interp.space)
+            arg =  s_frame.peek(argcount - 1).unwrap_longlong(interp.space)
+            if (rcvr  < 0 and arg >= 0) or (rcvr >= 0 and arg < 0):
+                raise PrimitiveFailedError
+            return primfunc(interp, s_frame, argcount)
         func.func_name = name
-        LargeIntegerPlugin.expose_primitive(unwrap_specs=[[int, int], [pos_32bit_int, pos_32bit_int], [r_uint, r_uint]])(func)
-    make_func(op)
+        LargeIntegerPlugin.expose_primitive(clean_stack=False, no_result=True)(func)
+    make_func(primitive)
