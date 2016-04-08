9cdcc585b1e2d91bb4ef5de3c32fdebc2791c1a6
Tim Felgentreff <timfelgentreff@gmail.com>
2016-04-08 12:11:05 +0200

    install coveralls locally on linux, globally on osx

--- .travis/install_requirements.sh
+++ .travis/install_requirements.sh
@@ -9,18 +9,20 @@ setup_osx() {
     ls /Volumes/*SDL*/
     sudo ditto /Volumes/*SDL*/SDL2.framework /Library/Frameworks/SDL2.framework
     # todo: Squeak for jittests
+    sudo pip install coveralls
 }
 
 setup_linux() {
     # on Linux, libsdl2 is installed through our dependencies stuff
-    pip install coveralls
     wget http://squeakvm.org/unix/release/Squeak-4.10.2.2614-linux_i386.tar.gz
     tar xzvf Squeak-4.10*.tar.gz
     rm Squeak-4.10*.tar.gz
     ln -s $PWD/Squeak-4.10*/bin/squeak .build/squeak
+    # also install coveralls
+    export PATH=.build/pypy-linux32/bin/:$PATH
+    pip install coveralls
 }
-
-setup_$TRAVIS_OS_NAME
 
 python .build/download_dependencies.py
 
+setup_$TRAVIS_OS_NAME
