f3393e5215f1dc278747b1222348ad6c1a837445
Tim Felgentreff <timfelgentreff@gmail.com>
2016-04-29 09:23:24 +0200

    split update and download image steps

--- scripts/get_image.sh
+++ scripts/get_image.sh
@@ -28,25 +28,4 @@ src=${src%%\"}
 curl -O "$SQUEAK_SERVER/$SOURCE_FOLDER/${src}"
 gunzip -f ${src}
 
-update_file="__squeak_update.st"
-cat <<EOF> $update_file
-TranscriptStream redirectToStdOut: true.
-Utilities setAuthorInitials: 'benchmarkerCI'.
-[[MCMcmUpdater updateFromServer]
-	on: ProvideAnswerNotification
-	do: [:n | n resume: true]]
-	on: Warning
-	do: [:n | n resume: true].
-[(Installer squeak
-	project: 'VMMaker')
-	install: 'VMMaker.oscog'] on: Warning do: [:n | n resume: true].
-(Installer swasource
-	project: 'BenchmarkRunner')
-	install: 'Benchmark';
-	install: 'SMark'.
-SystemWindow allSubInstancesDo: [:w | w delete].
-(SmalltalkImage current snapshot: true andQuit: false) ifFalse: [
-        SmalltalkImage current snapshot: false andQuit: true].
-EOF
-cogspurlinux/squeak Squeak*.image $update_file
-rm $update_file
+exec $(dirname $0)/update_image.sh
