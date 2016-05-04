8a8f70ad6b217aadb0bb4e1c355de84581373704
Fabio Niephaus <code@fniephaus.com>
2016-05-04 09:20:31 +0200

    Move SQPyte methods from RSqueak interpreter back to database.py again

--- rsqueakvm/primitives/database.py
+++ rsqueakvm/primitives/database.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from rsqueakvm.constants import SYSTEM_ATTRIBUTE_IMAGE_NAME_INDEX
 from rsqueakvm.error import PrimitiveFailedError
 from rsqueakvm.primitives import expose_primitive
 from rsqueakvm.primitives.bytecodes import *
@@ -8,13 +9,57 @@ from rpython.rlib import jit
 from rpython.rtyper.lltypesystem import rffi
 
 from sqpyte.capi import CConfig
+from sqpyte.interpreter import Sqlite3DB, SQPyteException, SqliteException
+
+
+class _SQPyteDB(object):
+    _db = None
+
+    def __init__(self):
+        pass
+
+    def execute(self, sql):
+        jit.promote(sql)
+        return self._db.execute(sql)
+
+    def connect(self, space):
+        # Expecting .db file with same name and in same directory as image
+        file = space.get_system_attribute(SYSTEM_ATTRIBUTE_IMAGE_NAME_INDEX)
+        file_end = len(file) - 6
+        assert file_end > 0
+        file = file[:file_end] + '.db'
+
+        # Open database
+        try:
+            print "Trying to connect to %s..." % file
+            self._db = Sqlite3DB(file)
+            print "Success"
+        except (SQPyteException, SqliteException) as e:
+            print e
+
+    def disconnected(self):
+        return self._db is None
+
+    def close(self):
+        if self.disconnected():
+            return False
+
+        self._db.close()
+        self._db = None
+        print "Disconnected"
+        return True
+
+db = _SQPyteDB()
 
 
 @expose_primitive(SQPYTE_EXECUTE, unwrap_spec=[object, str])
 def sqpyte_execute(interp, s_frame, w_rcvr, sql_statement):
     space = interp.space
 
-    query = interp.db_execute(sql_statement)
+    if db.disconnected():
+        db.connect(space)
+
+    query = db.execute(sql_statement)
 
     # Fetch first row
     rc = query.mainloop()
@@ -38,8 +83,9 @@ def fetch_one_row(query, space, num_cols):
         typ = query.column_type(i)
         if typ == CConfig.SQLITE_TEXT or typ == CConfig.SQLITE_BLOB:
             textlen = query.column_bytes(i)
-            result = rffi.charpsize2str(
-                    rffi.cast(rffi.CCHARP, query.column_text(i)), textlen)
+            result = rffi.charpsize2str(rffi.cast(rffi.CCHARP,
+                                                  query.column_text(i)),
+                                        textlen)
             w_result = space.wrap_string(result)  # no encoding
         elif typ == CConfig.SQLITE_INTEGER:
             result = query.column_int64(i)
@@ -57,7 +103,7 @@ def fetch_one_row(query, space, num_cols):
 
 @expose_primitive(SQPYTE_CLOSE, unwrap_spec=[object, str])
 def sqpyte_close(interp, s_frame, w_rcvr, sql_statement):
-    return interp.space.wrap_bool(interp.db_close())
+    return interp.space.wrap_bool(db.close())
 
 
 ###############################################################################
