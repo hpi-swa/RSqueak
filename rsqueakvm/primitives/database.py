e13319d84ee0bf43e368db814a5c779b7f2fdd56
Fabio Niephaus <code@fniephaus.com>
2016-05-10 18:03:59 +0200

    Reimplement four primitives for SQPyte and add DBManager

--- rsqueakvm/primitives/database.py
+++ rsqueakvm/primitives/database.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from rsqueakvm.constants import SYSTEM_ATTRIBUTE_IMAGE_NAME_INDEX
 from rsqueakvm.error import PrimitiveFailedError
 from rsqueakvm.primitives import expose_primitive
 from rsqueakvm.primitives.bytecodes import *
 
-from rpython.rlib import jit
+# from rpython.rlib import jit
 from rpython.rtyper.lltypesystem import rffi
 
 from sqpyte.capi import CConfig
@@ -13,26 +12,19 @@ from sqpyte.interpreter import Sqlite3DB, SQPyteException, SqliteException
 
 
 class _SQPyteDB(object):
-    _db = None
 
-    def __init__(self):
-        pass
+    def __init__(self, filename):
+        self.connect(filename)
 
     def execute(self, sql):
-        jit.promote(sql)
+        # jit.promote(sql)
         return self._db.execute(sql)
 
-    def connect(self, space):
-        # Expecting .db file with same name and in same directory as image
-        file = space.get_system_attribute(SYSTEM_ATTRIBUTE_IMAGE_NAME_INDEX)
-        file_end = len(file) - 6
-        assert file_end > 0
-        file = file[:file_end] + '.db'
-
+    def connect(self, filename):
         # Open database
         try:
-            print "Trying to connect to %s..." % file
-            self._db = Sqlite3DB(file)
+            print "Trying to connect to %s..." % filename
+            self._db = Sqlite3DB(filename)
             print "Success"
         except (SQPyteException, SqliteException) as e:
             print e
@@ -49,37 +41,90 @@ class _SQPyteDB(object):
         print "Disconnected"
         return True
 
-db = _SQPyteDB()
+
+class _SQPyteCursor(object):
+    def __init__(self, cursor):
+        self.cursor = cursor
+        self.num_cols = 0
+        self.rc = 0
+
+    def next(self):
+        self.rc = self.cursor.mainloop()
+
+        if not self.num_cols:
+            self.num_cols = self.cursor.data_count()
+
+        if self.rc != CConfig.SQLITE_ROW:
+            return None
+
+        return self.cursor
 
 
-@expose_primitive(SQPYTE_EXECUTE, unwrap_spec=[object, str])
-def sqpyte_execute(interp, s_frame, w_rcvr, sql_statement):
-    space = interp.space
+class _DBManager(object):
+    _db_count = 0
+    _dbs = {}
+    _cursor_count = 0
+    _cursors = {}
 
-    if db.disconnected():
-        db.connect(space)
+    def __init__(self):
+        pass
 
-    query = db.execute(sql_statement)
+    def connect(self, filename):
+        pointer = self._db_count
+        self._dbs[pointer] = _SQPyteDB(filename)
 
-    # Fetch first row
-    rc = query.mainloop()
-    num_cols = query.data_count()
+        self._db_count += 1
 
-    rows = []
-    # Fetch all other rows
-    while rc == CConfig.SQLITE_ROW:
-        row = fetch_one_row(query, space, num_cols)
-        row_w = space.wrap_list(row)
-        rows.append(row_w)
-        rc = query.mainloop()
+        return pointer
 
-    return space.wrap_list(rows)
+    def execute(self, db_pointer, sql_statement):
+        db = self._dbs.get(db_pointer, None)
+        if not db:
+            raise PrimitiveFailedError
+
+        pointer = self._cursor_count
+        self._cursors[pointer] = _SQPyteCursor(db.execute(sql_statement))
+
+        self._cursor_count += 1
+
+        return pointer
+
+    def cursor(self, cursor_pointer):
+        return self._cursors.get(cursor_pointer, None)
+
+    def close(self, db_pointer):
+        db = self._dbs.get(db_pointer, None)
+        if not db:
+            raise PrimitiveFailedError
+        return db.close()
 
 
-@jit.unroll_safe
-def fetch_one_row(query, space, num_cols):
-    cols = [None] * num_cols
-    for i in range(num_cols):
+dbm = _DBManager()
+
+
+@expose_primitive(SQPYTE_CONNECT, unwrap_spec=[object, str])
+def sqpyte_connect(interp, s_frame, w_rcvr, filename):
+    return interp.space.wrap_int(dbm.connect(filename))
+
+
+@expose_primitive(SQPYTE_EXECUTE, unwrap_spec=[object, int, str])
+def sqpyte_execute(interp, s_frame, w_rcvr, db_pointer, sql_statement):
+    return interp.space.wrap_int(dbm.execute(db_pointer, sql_statement))
+
+
+@expose_primitive(SQPYTE_NEXT, unwrap_spec=[object, int])
+def sqpyte_next(interp, s_frame, w_rcvr, cursor_pointer):
+    cursor = dbm.cursor(cursor_pointer)
+    if not cursor:
+        return interp.space.w_nil
+    row = fetch_one_row(cursor, interp.space)
+    return interp.space.wrap_list(row)
+
+
+def fetch_one_row(cursor, space):
+    query = cursor.next()
+    cols = [None] * cursor.num_cols
+    for i in range(cursor.num_cols):
         typ = query.column_type(i)
         if typ == CConfig.SQLITE_TEXT or typ == CConfig.SQLITE_BLOB:
             textlen = query.column_bytes(i)
@@ -101,9 +146,9 @@ def fetch_one_row(query, space, num_cols):
     return cols
 
 
-@expose_primitive(SQPYTE_CLOSE, unwrap_spec=[object, str])
-def sqpyte_close(interp, s_frame, w_rcvr, sql_statement):
-    return interp.space.wrap_bool(db.close())
+@expose_primitive(SQPYTE_CLOSE, unwrap_spec=[object, int])
+def sqpyte_close(interp, s_frame, w_rcvr, db_pointer):
+    return interp.space.wrap_bool(dbm.close(db_pointer))
 
 
 ###############################################################################
