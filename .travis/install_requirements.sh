a8413a73b3fbdf6f18f103f4d034cdc2eb490325
Fabio Niephaus <code@fniephaus.com>
2016-05-12 19:57:15 +0200

    Debug

--- .travis/install_requirements.sh
+++ .travis/install_requirements.sh
@@ -124,7 +124,8 @@ presetup_$TRAVIS_OS_NAME
 python .build/download_dependencies.py || true
 setup_$TRAVIS_OS_NAME
 
+ls .build/
 # build sqlite in sqpyte project
-pushd ".build/sqpyte" > /dev/null
+pushd ".build/sqpyte/" > /dev/null
 make
 popd > /dev/null
