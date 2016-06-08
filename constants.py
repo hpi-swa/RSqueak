920dea38c2e3703b926464c19e6be9bf08b300e1
Tim Felgentreff <timfelgentreff@gmail.com>
2016-06-08 10:46:09 +0000

    moving codespeed

--- constants.py
+++ constants.py
@@ -4,7 +4,8 @@ API_PORT = 8082
 QUEUE_PORT = 8083
 CONTROL_PORT = 8084
 BENCHMARK_MACHINES = [
-    "http://fb12ce8ws06:%s" % QUEUE_PORT
+    "http://fb12ce8ws06:%s" % QUEUE_PORT,
+    "http://timfelgentreff.no-ip.org:%s" % QUEUE_PORT
 ]
 JOB_TABLE = "jobs"
 COMMITID = "commitid"
@@ -12,7 +13,7 @@ BRANCH = "branch"
 VM = "vm"
 FLAG = "done"
 DBFILE = "benchmarks.db"
-CODESPEED_URL = 'http://172.16.64.134/'
+CODESPEED_URL = 'http://speed.squeak.org/'
 BINARY_URL = "https://www.hpi.uni-potsdam.de/hirschfeld/artefacts/rsqueak/commits/{}"
 BINARY_BASENAME = "rsqueak-x86-linux-jit-{}"
 
