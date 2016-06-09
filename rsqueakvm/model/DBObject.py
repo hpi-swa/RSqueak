491c50d44c033a803f72314150d5a82488796cf6
Philipp Otto <philipp.4096@gmail.com>
2016-06-09 17:16:29 +0200

    further work on DBObject

--- rsqueakvm/model/DBObject.py
+++ rsqueakvm/model/DBObject.py
@@ -6,21 +6,43 @@ import pdb
 
 class W_DBObject(W_PointersObject):
 
-    # map from className to db pointer
-    aMap = {"TestDBObject": 0}
+    db_connection = None
+    db_pointers = {}
 
     @jit.unroll_safe
     def __init__(self, space, w_class, size, weak=False):
         super(W_DBObject, self).__init__(space, w_class, size, weak)
-        pdb.set_trace()
-        # does db pointer exist?
-            # create table
+        self.column_names = {}
+
+        if not W_DBObject.db_connection:
+            print("Establish connection")
+            W_DBObject.db_connection = "New Connection"
+
+        self.class_name = w_class.classname(space)
+        if not self.class_name in W_DBObject.db_pointers:
+            print("CREATE TABLE", self.class_name, "(id integer)")
+            W_DBObject.db_pointers[self.class_name] = 1337
 
 
     def fetch(self, space, n0):
-        pdb.set_trace()
+        print("Fetch in", self.class_name, n0)
         return self._get_strategy().fetch(self, n0)
 
     def store(self, space, n0, w_value):
-        pdb.set_trace()
+
+        if not n0 in self.column_names:
+            aType = "blob"
+            w_value_type = type(w_value)
+            # TODO: type checking does not work this way, since primitive types
+            # are wrapped
+            if w_value_type == int:
+                aType = "integer"
+            elif w_value_type == str:
+                aType = "text"
+            elif w_value_type == float:
+                aType = "real"
+            print("alter table", self.class_name, "add column", '"' + str(n0) + '"', aType)
+            self.column_names[n0] = True
+
+        print("Store in", self.class_name, n0, w_value)
         return self._get_strategy().store(self, n0, w_value)
