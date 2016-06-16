54107f8fac3e136ebe9ff7e8029808b717a379e6
Philipp Otto <philipp.4096@gmail.com>
2016-06-16 16:46:25 +0200

    allow to save DBObjects within DBObjects

--- rsqueakvm/model/DBObject.py
+++ rsqueakvm/model/DBObject.py
@@ -4,14 +4,16 @@ from rpython.rlib import objectmodel, jit
 from rsqueakvm.plugins.database import SQLConnection
 from sqpyte import interpreter
 from rsqueakvm.error import PrimitiveFailedError
-import pdb
 
 
 class W_DBObject(W_PointersObject):
 
     db_connection = None
     id_counter = 0
-    column_names_for_table = {}
+    column_types_for_table = {}
+    # Maps from DBObject id to DBObject and only includes DBObjects which are
+    # referenced from an attribute of a DBObject.
+    db_objects = {}
 
     @jit.unroll_safe
     def __init__(self, space, w_class, size, weak=False):
@@ -26,8 +28,8 @@ class W_DBObject(W_PointersObject):
 
         # remove " class" from the classname
         self.class_name = w_class.classname(space).split(" ")[0]
-        if not self.class_name in W_DBObject.column_names_for_table:
-            W_DBObject.column_names_for_table[self.class_name] = {}
+        if not self.class_name in W_DBObject.column_types_for_table:
+            W_DBObject.column_types_for_table[self.class_name] = {}
 
         create_sql = "CREATE TABLE IF NOT EXISTS %s (id INTEGER);" % self.class_name
         print create_sql
@@ -36,21 +38,27 @@ class W_DBObject(W_PointersObject):
         print insert_sql
         W_DBObject.db_connection.execute(insert_sql, [self.w_id])
 
+    def get_column_types(self):
+        return W_DBObject.column_types_for_table[self.class_name]
 
     def fetch(self, space, n0):
-        print("Fetch in", self.class_name, n0)
         query_sql = "SELECT inst_var_%s FROM %s WHERE id=?;" % (n0, self.class_name)
+        print query_sql
         cursor = W_DBObject.db_connection.execute(query_sql, [self.w_id])
 
         w_result = space.unwrap_array(cursor.next())
         if w_result:
-            return w_result[0]
+            if self.get_column_types()[n0] == "blob":
+                db_id = space.unwrap_int(w_result[0])
+                return W_DBObject.db_objects[db_id]
+            else:
+                return w_result[0]
         else:
             raise PrimitiveFailedError
 
     def store(self, space, n0, w_value):
 
-        aType = "blob"
+        aType = None
         cls = w_value.getclass(space)
         if (cls.is_same_object(space.w_String)):
             aType = "text"
@@ -59,17 +67,23 @@ class W_DBObject(W_PointersObject):
         elif cls.is_same_object(space.w_Float):
             aType = "real"
         elif cls.is_same_object(space.w_nil):
-            return
+            aType = None
         else:
-            raise PrimitiveFailedError(
-                'unable to unwrap %s' % w_value.getclass(space))
+            if isinstance(w_value, W_DBObject):
+                aType = "blob"
+                W_DBObject.db_objects[w_value.id] = w_value
+                # Save id in database.
+                w_value = w_value.w_id
+            else:
+                raise PrimitiveFailedError(
+                    'unable to unwrap %s' % w_value.getclass(space))
 
-        if not n0 in W_DBObject.column_names_for_table[self.class_name]:
+        if aType and not n0 in self.get_column_types():
             alter_sql = "alter table %s add column inst_var_%s %s;" % (self.class_name, n0, aType)
             print alter_sql
             W_DBObject.db_connection.execute(alter_sql)
 
-            W_DBObject.column_names_for_table[self.class_name][n0] = True
+            self.get_column_types()[n0] = aType
 
         update_sql = "update %s set inst_var_%s=? where id=?" % (self.class_name, n0)
         print update_sql
