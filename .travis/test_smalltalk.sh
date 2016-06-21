c155370a18438ebc82ac4d91df18f25f8ce44ed6
Fabio Niephaus <code@fniephaus.com>
2016-06-21 10:38:49 +0200

    Make test_smalltalk.sh compatible to OS X

--- .travis/test_smalltalk.sh
+++ .travis/test_smalltalk.sh
@@ -2,13 +2,15 @@
 
 set -e
 
-IMAGE=${IMAGE:-}
-IMAGE_URL=${IMAGE_URL:-"https://www.hpi.uni-potsdam.de/hirschfeld/artefacts/rsqueak/bundle/RSqueak.tar.gz"}
-IMAGE_DIR=${IMAGE_DIR:-}
-IMAGE_PATH=${IMAGE_PATH:-"./RSqueak.app/Contents/Resources/RSqueak.image"}
-IMAGE_EXTRACT=${IMAGE_EXTRACT:-"./RSqueak.app/Contents/Resources/"}
-RESULT_CMD=${RESULT_CMD:-}
-RESULT_CMD_URL=${RESULT_CMD_URL:-"https://raw.githubusercontent.com/hpi-swa/smalltalkCI/master/lib/junit_xml_prettfier.py"}
+IMAGE="${IMAGE:-}"
+IMAGE_URL="${IMAGE_URL:-"https://www.hpi.uni-potsdam.de/hirschfeld/artefacts/\
+rsqueak/bundle/RSqueak.tar.gz"}"
+IMAGE_DIR="${IMAGE_DIR:-}"
+IMAGE_EXTRACT="${IMAGE_EXTRACT:-"./RSqueak.app/Contents/Resources"}"
+IMAGE_PATH="${IMAGE_EXTRACT}/RSqueak.image"
+RESULT_CMD="${RESULT_CMD:-}"
+RESULT_CMD_URL="${RESULT_CMD_URL:-"https://raw.githubusercontent.com/hpi-swa/\
+smalltalkCI/master/lib/junit_xml_prettfier.py"}"
 
 realpath() {
     [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
@@ -28,8 +30,10 @@ read -d '' CODE <<EOF || true
     SCISqueakTestReport runClasses: {SQLiteTests. SQPyteTests} named: 'test'.
 EOF
 
-if [ -z "${IMAGE}" ]; then
-    IMAGE_DIR=${IMAGE_DIR:-$(mktemp -d --suffix -sqpyte-test)}
+if [[ -z "${IMAGE}" ]]; then
+    if [[ -z "${IMAGE_DIR}" ]]; then
+        IMAGE_DIR=$(mktemp -d "${TMPDIR:-/tmp}/sqpyte.XXXXXXXXX")
+    fi
     TAR="${IMAGE_DIR}/rsqueak.tar.gz"
 
     mkdir -p "${IMAGE_DIR}"
@@ -41,7 +45,7 @@ if [ -z "${IMAGE}" ]; then
     fi
 
     echo "==== Extract archive..."
-    tar -zxf "${TAR}" "${IMAGE_EXTRACT}"
+    tar -zxf "${TAR}" "${IMAGE_EXTRACT}/"
     IMAGE=$(realpath "${IMAGE_PATH}")
 
     popd
@@ -49,23 +53,22 @@ else
     IMAGE_DIR=$(dirname "${IMAGE}")
 fi
 
-if [ ! -f "${IMAGE}" ]; then
-    echo "Image file not found. That's bad."
-    exit 1
+if [[ ! -f "${IMAGE}" ]]; then
+    echo "Image file not found. That's bad." && exit 1
 fi
 
 echo "==== Run tests..."
 ./rsqueak -r "${CODE}" "${IMAGE}"
 
-if [ -z "${RESULT_CMD}" ]; then
+if [[ -z "${RESULT_CMD}" ]]; then
     RESULT_CMD="/tmp/print_test_results.py"
 
-    if [ ! -f "${RESULT_CMD}" ]; then
+    if [[ ! -f "${RESULT_CMD}" ]]; then
         wget --quiet "${RESULT_CMD_URL}" -O "${RESULT_CMD}"
         chmod +x "${RESULT_CMD}"
     fi
 fi
 
-if [ ! -z "${RESULT_CMD}" ]; then
+if [[ ! -z "${RESULT_CMD}" ]]; then
     "${RESULT_CMD}" "${IMAGE_DIR}"
 fi
