e56eb93ed7ead40f63c4d1f134d24d2cc6c978be
Fabio Niephaus <code@fniephaus.com>
2016-07-27 00:37:03 +0200

    Do not mock db driver and skip tests if necessary

--- rsqueakvm/test/test_database.py
+++ rsqueakvm/test/test_database.py
@@ -1,40 +1,37 @@
+import platform
 import py
+import pytest
 
-from rsqueakvm.database import dbm, SQLCursor
+from rsqueakvm.database import dbm
 from rsqueakvm.error import PrimitiveFailedError
 
-from .util import create_space
+from sqpyte.interpreter import SQPyteDB
 
 
-class FakeDBDriver():
+def _is_incompatible():
+    try:
+        import sqpyte
+        return "64bit" not in platform.architecture()[0]
+    except ImportError:
+        return True
 
-    def __init__(self, filename):
-        self.filename = filename
-
-    def execute(self, sql, use_flag_cache=True):
-        pass
-
-    def close(self):
-        pass
-
-
-def setup_module():
-    space = create_space(bootstrap = True)
+skipif_incompatible = pytest.mark.skipif(
+    _is_incompatible(), reason="64bit required or sqpyte not found")
 
 
+@skipif_incompatible
 def test_db_manager_primitives(monkeypatch):
-    monkeypatch.setattr(SQLCursor, "_step", lambda s: None)
     py.test.raises(PrimitiveFailedError, lambda: dbm.get_connection('foo'))
-    db_handle = dbm.connect(FakeDBDriver, 'test.db')
+    db_handle = dbm.connect(SQPyteDB, 'test.db')
     assert db_handle == 0
     db = dbm.get_connection(db_handle)
-    cursor_handle = dbm.execute(None, db, 'sql statement')
+    cursor_handle = dbm.execute(None, db, 'SELECT 1;')
     assert cursor_handle == 0
     monkeypatch.undo()
 
 
+@skipif_incompatible
 def test_db_manager_db_object():
-    dbm.driver = FakeDBDriver
     conn1 = dbm.connection()
     conn2 = dbm.connection()
     assert conn1 is conn2
