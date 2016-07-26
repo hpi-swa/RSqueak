ac602f330ddb26bc4725dc3ac428f94bb752f5c1
Fabio Niephaus <code@fniephaus.com>
2016-07-26 16:55:12 +0200

    Extend benchmarks with KMeans and allInstancesDo:
    [ci skip]

--- scripts/bench_sqpyte.sh
+++ scripts/bench_sqpyte.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Required Squeak packages: Obsidian-Elo, ORM, SQPyte-Core
+# Required Squeak packages: KMeans, Obsidian-Elo, ORM, SQPyte-Core
 
 if [ "$#" -ne 1 ]; then
   echo "Please provide an image!"
@@ -12,6 +12,7 @@ MAX=10000
 STEPS=2000
 MATCHES=2000
 
+echo "#### Running Elo Benchmarks..."
 while [ ${MATCHES} -le ${MAX} ]; do
   ./rsqueak --silent -r "SQLElo testPreparedElo: ${MATCHES} usingSQPyte: False" "${IMAGE}"
   echo "for ${MATCHES} matches with RFFI+SQLite"
@@ -19,14 +20,38 @@ while [ ${MATCHES} -le ${MAX} ]; do
   ./rsqueak --silent -r "SQLElo testPreparedElo: ${MATCHES} usingSQPyte: True" "${IMAGE}"
   echo "for ${MATCHES} matches with RFFI+SQPyte"
   echo "======================================================================="
-  ./rsqueak --silent -r "|obj| DBObject Mode: 0. obj := (EloBenchmark new) setUp: ${MATCHES}. ^ [obj benchElo] timeToRun" "${IMAGE}"
+  ./rsqueak --silent -r "|b| DBObject Mode: 0. b := (EloBenchmark new) setUp: ${MATCHES}. ^ [b benchElo] timeToRun" "${IMAGE}"
   echo "for ${MATCHES} matches without database"
   echo "======================================================================="
-  ./rsqueak --silent -r "|obj| DBObject Mode: 1. obj := (EloBenchmark new) setUp: ${MATCHES}. ^ [obj benchElo] timeToRun" "${IMAGE}"
+  ./rsqueak --silent -r "|b| DBObject Mode: 1. b := (EloBenchmark new) setUp: ${MATCHES}. ^ [b benchElo] timeToRun" "${IMAGE}"
   echo "for ${MATCHES} matches with DBObject+SQLite"
   echo "======================================================================="
-  ./rsqueak --silent -r "|obj| DBObject Mode: 2. obj := (EloBenchmark new) setUp: ${MATCHES}. ^ [obj benchElo] timeToRun" "${IMAGE}"
+  ./rsqueak --silent -r "|b| DBObject Mode: 2. b := (EloBenchmark new) setUp: ${MATCHES}. ^ [b benchElo] timeToRun" "${IMAGE}"
   echo "for ${MATCHES} matches with DBObject+SQPyte"
   echo "======================================================================="
+
+  ./rsqueak -r "|b| DBObject Mode: 0. b := (EloBenchmark new) setUp: ${MATCHES}. ^ [b benchEloWithAllInstances] timeToRun." "${IMAGE}"
+  echo "for ${MATCHES} matches without database and with Object>>allInstancesDo:"
+  echo "======================================================================="
+  ./rsqueak -r "|b| DBObject Mode: 1. b := (EloBenchmark new) setUp: ${MATCHES}. ^ [b benchEloWithAllInstances] timeToRun." "${IMAGE}"
+  echo "for ${MATCHES} matches with RFFI+SQLite and DBObject>>allInstancesDo:"
+  echo "======================================================================="
+  ./rsqueak -r "|b| DBObject Mode: 2. b := (EloBenchmark new) setUp: ${MATCHES}. ^ [b benchEloWithAllInstances] timeToRun." "${IMAGE}"
+  echo "for ${MATCHES} matches with RFFI+SQPyte and DBObject>>allInstancesDo:"
+  echo "======================================================================="
+
   MATCHES=$((${MATCHES}+${STEPS}))
 done
+
+echo ""
+
+echo "#### Running KMeans Benchmarks..."
+./rsqueak --silent -r "|b| DBObject Mode: 0. b := KMeansBenchmark new. b setup. ^ [b run] timeToRun" "${IMAGE}"
+echo "KMeansBenchmark without database"
+echo "======================================================================="
+./rsqueak --silent -r "|b| DBObject Mode: 1. b := KMeansBenchmark new. b setup. ^ [b run] timeToRun" "${IMAGE}"
+echo "KMeansBenchmark with DBObject+SQLite"
+echo "======================================================================="
+./rsqueak --silent -r "|b| DBObject Mode: 2. b := KMeansBenchmark new. b setup. ^ [b run] timeToRun" "${IMAGE}"
+echo "KMeansBenchmark with DBObject+SQPyte"
+echo "======================================================================="
