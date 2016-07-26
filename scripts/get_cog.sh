0b121a7b274b90ff4cf8b42415f81d0a144e6e52
Tim Felgentreff <timfelgentreff@gmail.com>
2016-07-26 09:38:01 +0200

    update cog scripts to download builds from bintray

--- scripts/get_cog.sh
+++ scripts/get_cog.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
+if ( echo $0 | grep 64 ); then
+    words=64
+    pkg=cog_linux64x64_squeak.cog.spur
+    product=cogspur64linuxht
+else
+    words=32
+    pkg=cog_linux32x86_squeak.cog.spur
+    product=cogspurlinuxht
+fi
+
+credentials="$(cat `dirname $0`/bintray.credentials | head -1)"
+
 if [ -n "$1" ]; then
-    echo "Downloading version ${1}"
-    COGURL="http://www.mirandabanda.org/files/Cog/VM/VM.r$1/"
+    version=$1
+    echo "Downloading version ${version}"
 else
-    echo "Downloading latest Cog VM"
-    COGURL="http://www.mirandabanda.org/files/Cog/VM/latest/"
+    version=$(curl -sL "https://${credentials}@api.bintray.com/packages/opensmalltalk/vm/cog" | grep -oP latest_version\"\:\"[0-9]+\" | grep -oP "[0-9]+")
+    echo "Downloading latest Cog VM: ${version}"
 fi
+COGURL="https://${credentials}@dl.bintray.com/opensmalltalk/vm/${pkg}_${version}.tar.gz"
 
-string="$(uname -m)"
-if [[ "$string" == *"arm"* ]]; then
-    zip=$(curl -s $COGURL | grep -o "href=\"cogspurlinuxhtARM-.*tgz\"" | tail -1)
-else
-    zip=$(curl -s $COGURL | grep -o "href=\"cogspurlinuxht-.*tgz\"" | tail -1)
-fi
+curl -sLO "$COGURL"
 
-zip=${zip#*=}
-zip=${zip#\"}
-zip=${zip%%\"}
-curl -O "$COGURL/$zip"
 for i in cogspurlinux*; do
     test -d $i && rm -rf $i
 done
-rm -rf cog32
-tar xzf $zip
-rm $zip
-mv cogspurlinux* tmpcog
-mv tmpcog cog32
+rm -rf cog${words}
+tar xzf ${pkg}_${version}.tar.gz
+rm ${pkg}_${version}.tar.gz
+mv products/${product} cog${words}
+rm -rf products
