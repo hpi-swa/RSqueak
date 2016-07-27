bb5df51a6358b0599c6076bcc114c05dc01802ca
Fabio Niephaus <code@fniephaus.com>
2016-07-27 12:11:13 +0200

    Merge W_DBbject tests into test_database.py
    and use skipif_incompatible decorator to fix
    Travis builds

--- rsqueakvm/test/test_database.py
+++ rsqueakvm/test/test_database.py
@@ -2,8 +2,13 @@ import platform
 import py
 import pytest
 
+from rsqueakvm import storage_classes
 from rsqueakvm.database import dbm
 from rsqueakvm.error import PrimitiveFailedError
+from rsqueakvm.model.database import W_DBObject
+from rsqueakvm.model.pointers import W_PointersObject
+
+from .util import create_space
 
 
 def _import_SQPyteDB():
@@ -20,8 +25,15 @@ skipif_incompatible = pytest.mark.skipif(
     SQPyteDB is None, reason="64bit required or sqpyte not found")
 
 
+def _bootstrap_class(instsize, name, w_superclass=None, w_metaclass=None,
+                     format=storage_classes.POINTERS, varsized=True):
+    space = create_space(bootstrap=True)
+    return space.bootstrap_class(instsize, w_superclass, w_metaclass,
+                                 name, format, varsized)
+
+
 @skipif_incompatible
-def test_db_manager_primitives(monkeypatch):
+def test_dbmanager_primitives(monkeypatch):
     py.test.raises(PrimitiveFailedError, lambda: dbm.get_connection('foo'))
     db_handle = dbm.connect(SQPyteDB, 'test.db')
     assert db_handle == 0
@@ -32,7 +44,43 @@ def test_db_manager_primitives(monkeypatch):
 
 
 @skipif_incompatible
-def test_db_manager_db_object():
+def test_dbmanager_db_object():
     conn1 = dbm.connection()
     conn2 = dbm.connection()
     assert conn1 is conn2
+
+
+@skipif_incompatible
+def test_dbobject_fetch_and_store_with_string():
+    space = create_space(bootstrap=True)
+    instsize = 10
+    obj = W_DBObject(space, _bootstrap_class(instsize, "Foo"), instsize)
+    val = "test-string"
+    obj.store(space, 0, space.wrap_string(val))
+    returned_val = obj.fetch(space, 0)
+
+    assert val == space.unwrap_string(returned_val)
+
+
+@skipif_incompatible
+def test_dbobject_fetch_and_store_with_pointers_object():
+    space = create_space(bootstrap=True)
+    instsize = 10
+    obj = W_DBObject(space, _bootstrap_class(instsize, "Foo"), instsize)
+    val = W_PointersObject(space, _bootstrap_class(instsize, "Foo"), instsize)
+    obj.store(space, 0, val)
+    returned_val = obj.fetch(space, 0)
+
+    assert val == returned_val
+
+
+@skipif_incompatible
+def test_dbobject_fetch_and_store_with_w_db_object():
+    space = create_space(bootstrap=True)
+    instsize = 10
+    obj = W_DBObject(space, _bootstrap_class(instsize, "Doo"), instsize)
+    val = W_DBObject(space, _bootstrap_class(instsize, "Bar"), instsize)
+    obj.store(space, 0, val)
+    returned_val = obj.fetch(space, 0)
+
+    assert val == returned_val
