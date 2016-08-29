d111cb7ea94f55b8f663f7dc3a112be80ec7c7fe
Tim Felgentreff <timfelgentreff@gmail.com>
2016-08-29 11:09:19 +0200

    for rsqueak, we'll run the codespeed harness if both linux builds passes, ignoring everything else

--- .travis.rb
+++ .travis.rb
@@ -15,7 +15,14 @@ vm = ENV["VM"]
 yesterdays_builds = []
 repo.builds.each do |b|
   break if (b.finished_at && b.finished_at < 1.day.ago)
-  yesterdays_builds << b if b.passed?
+  if vm == "rsqueak"
+    # if both linux builds worked, we're good to go
+    if b.jobs.select { |j| j.config["os"] == "linux" && j.config["env"] =~ /^BUILD_ARCH=[63][42]bit$/ }.all?(&:passed?)
+      yesterdays_builds << b
+    end
+  else
+    yesterdays_builds << b if b.passed?
+  end
 end
 
 yesterdays_builds.group_by do |b|
