d8622a9ad99516c202257823aa2913d6b91d005f
Tim Felgentreff <timfelgentreff@gmail.com>
2016-10-19 14:23:50 +0200

    [rbigint] just go for rbigint operations in large integers plugin

--- rsqueakvm/plugins/large_integers.py
+++ rsqueakvm/plugins/large_integers.py
@@ -3,6 +3,8 @@ from rsqueakvm.plugins.plugin import Plugin
 from rsqueakvm.primitives import prim_table
 from rsqueakvm.primitives.constants import *
 
+from rpython.rlib.rbigint import rbigint
+
 LargeIntegers = Plugin()
 
 bitops = {
@@ -36,23 +38,18 @@ for name, primitive in negOps.items():
         LargeIntegers.expose_primitive()(func)
     make_func(primitive)
 
+@SqueakSSL.expose_primitive(unwrap_spec=[object, object, str, index1_0, int, object])
+def primitiveDecrypt(interp, s_frame, w_rcvr, w_handle, src, start, srclen, w_dst):
+
+
 ops = {
-    'primDigitAdd': ADD,
-    'primDigitSubtract': SUBTRACT,
+    'primDigitAdd': rbigint.add,
+    'primDigitSubtract': rbigint.sub,
 }
-for name, primitive in ops.items():
+for name, op in ops.items():
     def make_func(primitive):
-        primfunc = prim_table[primitive]
-        def func(interp, s_frame, argcount):
-            """This operation is only supported for sign-matching
-            receiver/argument pairs."""
-            if 2 < argcount or argcount < 1:
-                raise PrimitiveFailedError
-            rcvr = s_frame.peek(argcount).unwrap_longlong(interp.space)
-            arg = s_frame.peek(argcount - 1).unwrap_longlong(interp.space)
-            if (rcvr < 0 and arg >= 0) or (rcvr >= 0 and arg < 0):
-                raise PrimitiveFailedError
-            return primfunc(interp, s_frame, argcount)
+        def func(interp, s_frame, rcvr, arg):
+            return space.wrap_rbigint(op(rcvr, arg))
         func.func_name = name
-        LargeIntegers.expose_primitive(clean_stack=False, no_result=True)(func)
+        LargeIntegers.expose_primitive(unwrap_spec=[rbigint, rbigint])(func)
     make_func(primitive)
