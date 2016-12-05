5b99d6b5b8046b13ea3b2a49a43b63865a5115b6
Tim Felgentreff <timfelgentreff@gmail.com>
2016-12-05 10:11:58 +0100

    test large integers plugin in numerical tower

--- rsqueakvm/test/test_numerical_tower.py
+++ rsqueakvm/test/test_numerical_tower.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import py, operator
 from collections import OrderedDict
+from rsqueakvm.model.pointers import W_PointersObject
 from rsqueakvm.primitives import prim_holder
+from rsqueakvm.primitives.constants import EXTERNAL_CALL
 from rsqueakvm.test.test_primitives import MockFrame
 from .util import read_image, copy_to_module, cleanup_module, create_space, slow_test
 
@@ -24,6 +26,17 @@ def perform_primitive(rcvr, w_selector, *args):
     func = prim_holder.prim_table[code]
     s_frame = MockFrame(space, [rcvr] + list(args)).as_context_get_shadow(space)
     func(interp, s_frame, len(args))
+    return s_frame.pop()
+
+def external_call(plugin, name, stack):
+    stack = [space.w(o) for o in stack]
+    w_description = W_PointersObject(space, space.classtable['w_Array'], 2)
+    w_description.atput0(space, 0, space.w(plugin))
+    w_description.atput0(space, 1, space.w(name))
+    func = prim_holder.prim_table[EXTERNAL_CALL]
+    s_frame = space.make_frame("not called", [w_description], space.w_nil, stack)[1]
+    s_frame._stack_ptr = len(stack)
+    func(interp, s_frame, len(stack) - 1, s_frame.w_method())
     return s_frame.pop()
 
 def _op_or_overflow(op, *args):
@@ -89,12 +102,28 @@ the_siege = list(reduce(operator.concat,
 the_small_siege = the_positive_siege[:len(the_positive_siege)/2]
 the_tiny_siege = the_positive_siege[: 2 * len(the_positive_siege) / 7]
 
-
 # I'm paranoid
 assert sum(the_siege) == 0
 
+def identity(x): return x
+operator.identity = identity
+operator.divmod = divmod
+def digitcmp(x, y):
+    if isinstance(x, int) and isinstance(y, int):
+        return cmp(x,y)
+    elif isinstance(y, int):
+        return 1
+    else:
+        return cmp(abs(x), abs(y))
+operator.digitcmp = digitcmp
+
 ops = {
-    'unary': {'abs': 'abs', 'inv': ('bitInvert', 'inv'), 'negated': ('negated', 'neg')},
+    'unary': {
+        'abs': 'abs',
+        'inv': ('bitInvert', 'inv'),
+        'negated': ('negated', 'neg'),
+        'normalize': ('normalize', 'identity'),
+    },
     'binary': {
         'plus': ('+', 'add'),
         'minus': ('-', 'sub'),
@@ -118,6 +147,17 @@ ops = {
     },
     'shift': {'lshift': ('<<', 'lshift'), 'rshift': ('>>', 'rshift')},
     'big': {'raisedTo': ('raisedTo:', 'pow')},
+    'plugin': {
+        'primDigitBitAnd': ('primDigitBitAnd', 'and_'),
+        'primDigitBitOr': ('primDigitBitOr', 'or_'),
+        'primDigitBitXor': ('primDigitBitXor', 'xor'),
+        'primDigitBitShiftMagnitude': ('primDigitBitShiftMagnitude', ('lshift', 'rshift')),
+        'primDigitAdd': ('primDigitAdd', 'add'),
+        'primDigitSubtract': ('primDigitSubtract', 'sub'),
+        'primDigitMultiplyNegative': ('primDigitMultiplyNegative', 'mul'),
+        'primDigitDivNegative': ('primDigitDivNegative', 'divmod'),
+        'primDigitCompare': ('primDigitCompare', 'digitcmp'),
+    }
 }
 
 
@@ -193,3 +233,43 @@ def test_invert_equivalence():
     assert \
         perform(w(sq32maxval), w_selector=sel("bitInvert32"), w_arguments=[]).unwrap_long_untranslated(space) == \
         perform(w(sq32maxval), w_selector=sel("bitXor:"), w_arguments=[w(mask)]).unwrap_long_untranslated(space)
+
+@py.test.mark.parametrize('operand1', the_small_siege)
+@py.test.mark.parametrize('op', ops['plugin'].values(), ids=ops['plugin'].keys())
+def test_plugin(op, operand1):
+    if isinstance(op[1], tuple):
+        st_op = op[0]
+        py_op, py_neg_op = op[1]
+    else:
+        st_op, py_op = op
+        py_neg_op = None
+    for operand2 in the_small_siege:
+
+        if st_op.endswith("Negative"):
+            args = [operand1, operand2, (operand1 ^ operand2) < 0]
+        else:
+            args = [operand1, operand2]
+
+        try:
+            st_res = external_call('LargeIntegers', st_op, args).unwrap_long_untranslated(space)
+        except AttributeError:
+            st_res = list([
+                w_o.unwrap_long_untranslated(space) for w_o in
+                external_call('LargeIntegers', st_op, args).unwrap_array(space)
+            ])
+
+        if st_op == "primDigitDivNegative":
+            """to get from rem to mod, we have to add operand2 if rem^operand2<0.
+            We then have to subtract one from the div part"""
+            if (st_res[1] ^ operand2) < 0:
+                st_res[1] += operand2
+                st_res[0] -= 1
+
+        if py_neg_op and operand2 < 0:
+            py_res = getattr(operator, py_neg_op)(operand1, operand2)
+        else:
+            py_res = getattr(operator, py_op)(operand1, operand2)
+
+        if isinstance(py_res, tuple):
+            py_res = list(py_res)
+        assert st_res == py_res
