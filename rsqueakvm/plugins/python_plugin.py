0682e1a11036bfb6e0b65919c4cb58de8e84c738
Fabio Niephaus <code@fniephaus.com>
2016-12-18 11:08:04 +0100

    Fix translation and misc improvements

--- rsqueakvm/plugins/python_plugin.py
+++ rsqueakvm/plugins/python_plugin.py
@@ -23,6 +23,7 @@ from pypy.objspace.std.floatobject import W_FloatObject as WP_FloatObject
 from pypy.objspace.std.bytesobject import W_BytesObject as WP_BytesObject
 from pypy.objspace.std.noneobject import W_NoneObject as WP_NoneObject
 from pypy.objspace.std.listobject import W_ListObject as WP_ListObject
+from pypy.module.__builtin__ import compiling as py_compiling
 
 from pypy.interpreter.baseobjspace import W_Root
 from pypy.interpreter.error import OperationError
@@ -33,6 +34,7 @@ _DO_NOT_RELOAD = True
 
 
 def _new_pypy_objspace():
+    import os
     import sys
 
     # This module is reloaded, but pypy_getudir has already been deleted
@@ -44,6 +46,13 @@ def _new_pypy_objspace():
     from pypy.config.pypyoption import get_pypy_config
     translating = sys.argv[0] != '.build/run.py'  # make better
     pypy_config = get_pypy_config(translating=translating)
+
+    # cpyext causes a lot of "Undefined symbols for architecture x86_64" errors
+    pypy_config.objspace.usemodules.cpyext = False
+
+    # disabling cffi backend for now, it also causes an undefined symbol error
+    pypy_config.objspace.usemodules._cffi_backend = False
+
     from pypy.config.pypyoption import enable_allworkingmodules
     from pypy.config.pypyoption import enable_translationmodules
     enable_allworkingmodules(pypy_config)
@@ -54,17 +63,46 @@ def _new_pypy_objspace():
     # ensures pypy_hooks has a .space
     pypy_config.objspace.usemodules.pypyjit = True
 
-    # PyPy needs threads
-    pypy_config.translation.thread = True
     pypy_config.translation.continuation = True
 
-    # pypy_config.objspace.usemodules.pyexpat = False
+    pypy_config.objspace.usemodules.pyexpat = False
+
+    # Copy over some options that should be the same in both configs
+    pypy_config.translation.make_jobs = system.translationconfig.make_jobs
+    if system.translationconfig.output is not None:
+        pypy_config.translation.output = system.translationconfig.output
+
+    # merge_configs(config, pypy_config, "RSqueak", "PyPy")
+
+    # PyPy needs threads
+    pypy_config.translation.thread = True
 
     # Python objectspace ctor is not Rpython so create it here and
     # encapsulate it inside the entry point with a closure.
     from pypy.objspace.std import StdObjSpace as PyStdObjSpace
-    return PyStdObjSpace(pypy_config)
-python_space = _new_pypy_objspace()
+
+    py_space = PyStdObjSpace(pypy_config)
+
+    # equivalent to the hack in app_main.py of PyPy, albiet interp-level.
+    w_sys = py_space.sys
+    w_modnames = w_sys.get("builtin_module_names")
+    w_in = py_space.contains(w_modnames, py_space.wrap("__pypy__"))
+    if not py_space.is_true(w_in):
+        rl = py_space.sys.get("setrecursionlimit")
+        py_space.call(rl, py_space.newlist([py_space.wrap(5000)]))
+
+    # Should always be able to import Python modules in CWD.
+    w_sys_path = py_space.getattr(w_sys, py_space.wrap("path"))
+    py_space.call_method(w_sys_path, 'append', py_space.wrap("."))
+
+    # Set sys.executable in PyPy -- some modules rely upon this existing.
+    py_space.setattr(w_sys, py_space.wrap("executable"),
+                     py_space.wrap(os.path.abspath(sys.argv[0])))
+    return py_space
+
+py_space = _new_pypy_objspace()
+py_globals = py_space.newdict()
+py_locals = py_space.newdict()
 
 # Patch-out virtualizables from Pypy so that translation works
 from pypy.module.pypyjit.interp_jit import PyFrame, PyPyJitDriver
@@ -97,7 +135,7 @@ def startup(space, argv):
     if w_python_class is None:
         w_python_class = space.w_nil.getclass(space)
     PythonPlugin.w_python_object_class.set(w_python_class)
-    python_space.startup()
+    py_space.startup()
 PluginStartupScripts.append(startup)
 
 
@@ -106,15 +144,15 @@ def wrap(interp, wp_object):
     # import pdb; pdb.set_trace()
     space = interp.space
     if isinstance(wp_object, WP_FloatObject):
-        return space.wrap_float(python_space.float_w(wp_object))
+        return space.wrap_float(py_space.float_w(wp_object))
     elif isinstance(wp_object, WP_IntObject):
-        return space.wrap_int(python_space.int_w(wp_object))
+        return space.wrap_int(py_space.int_w(wp_object))
     elif isinstance(wp_object, WP_BytesObject):
-        return space.wrap_string(python_space.str_w(wp_object))
+        return space.wrap_string(py_space.str_w(wp_object))
     elif isinstance(wp_object, WP_ListObject):
         return space.wrap_list(
             [wrap(interp, item) for item in wp_object.getitems()])
-    elif isinstance(wp_object, WP_NoneObject):
+    elif wp_object is None or isinstance(wp_object, WP_NoneObject):
         return space.w_nil
     elif wp_object is WP_BoolObject.w_False:
         return space.w_false
@@ -131,12 +169,12 @@ def unwrap(interp, w_object):
     if isinstance(w_object, W_PythonObject):
         return w_object.wp_object
     elif isinstance(w_object, W_Float):
-        return python_space.newfloat(space.unwrap_float(w_object))
+        return py_space.newfloat(space.unwrap_float(w_object))
     elif isinstance(w_object, W_SmallInteger):
-        return python_space.newint(space.unwrap_int(w_object))
+        return py_space.newint(space.unwrap_int(w_object))
     elif isinstance(w_object, W_BytesObject):
         # if w_object.getclass(space).is_same_object(space.w_String):
-        return python_space.newbytes(space.unwrap_string(w_object))
+        return py_space.newbytes(space.unwrap_string(w_object))
     raise PrimitiveFailedError
 
 
@@ -150,10 +188,10 @@ class W_PythonObject(W_AbstractObjectWithIdentityHash):
         self.s_class = None
 
     def getclass(self, space):
-        return W_PythonObject(self.wp_object.getclass(python_space))
+        return W_PythonObject(self.wp_object.getclass(py_space))
 
     def class_shadow(self, space):
-        wp_class = python_space.type(self.wp_object)
+        wp_class = py_space.type(self.wp_object)
         return W_PythonObject.pure_class_shadow(space, wp_class)
 
     @staticmethod
@@ -216,46 +254,56 @@ class PythonClassShadow(ClassShadow):
         return w_cm
 
 
-@PythonPlugin.expose_primitive(unwrap_spec=[object, str])
-def eval(interp, s_frame, w_rcvr, source):
+@PythonPlugin.expose_primitive(unwrap_spec=[object, str, str])
+def eval(interp, s_frame, w_rcvr, source, cmd):
     try:
-        fake_globals = python_space.newdict()
-        fake_locals = python_space.newdict()
-        pycode = main.compilecode(python_space, source, '<string>', 'eval')
-        retval = pycode.exec_code(python_space, fake_globals, fake_locals)
+        # import pdb; pdb.set_trace()
+        wp_source = py_space.wrap(source)
+        py_code = py_compiling.compile(py_space, wp_source, '<string>', cmd)
+        retval = py_code.exec_code(py_space, py_globals, py_locals)
         return wrap(interp, retval)
     except OperationError as operationerr:
-        print operationerr.errorstr(python_space)
+        print operationerr.errorstr(py_space)
         raise PrimitiveFailedError
+
+
+@PythonPlugin.expose_primitive(unwrap_spec=[object, str])
+def getGlobal(interp, s_frame, w_rcvr, key):
+    return wrap(interp, py_globals.getitem(py_space.wrap(key)))
+
+
+@PythonPlugin.expose_primitive(unwrap_spec=[object, str])
+def getLocal(interp, s_frame, w_rcvr, key):
+    return wrap(interp, py_locals.getitem(py_space.wrap(key)))
 
 
 def _call_method(interp, wp_rcvr, methodname, args_w):
     args_w_len = len(args_w)
     if args_w_len == 1:
         arg1 = unwrap(interp, args_w[0])
-        return wrap(interp, python_space.call_method(wp_rcvr, methodname, arg1))
+        return wrap(interp, py_space.call_method(wp_rcvr, methodname, arg1))
     elif args_w_len == 2:
         arg1 = unwrap(interp, args_w[0])
         arg2 = unwrap(interp, args_w[1])
-        return wrap(interp, python_space.call_method(wp_rcvr, methodname, arg1, arg2))
+        return wrap(interp, py_space.call_method(wp_rcvr, methodname, arg1, arg2))
     elif args_w_len == 3:
         arg1 = unwrap(interp, args_w[0])
         arg2 = unwrap(interp, args_w[1])
         arg3 = unwrap(interp, args_w[2])
-        return wrap(interp, python_space.call_method(wp_rcvr, methodname, arg1, arg2, arg3))
+        return wrap(interp, py_space.call_method(wp_rcvr, methodname, arg1, arg2, arg3))
     elif args_w_len == 4:
         arg1 = unwrap(interp, args_w[0])
         arg2 = unwrap(interp, args_w[1])
         arg3 = unwrap(interp, args_w[2])
         arg4 = unwrap(interp, args_w[3])
-        return wrap(interp, python_space.call_method(wp_rcvr, methodname, arg1, arg2, arg3, arg4))
+        return wrap(interp, py_space.call_method(wp_rcvr, methodname, arg1, arg2, arg3, arg4))
     elif args_w_len == 5:
         arg1 = unwrap(interp, args_w[0])
         arg2 = unwrap(interp, args_w[1])
         arg3 = unwrap(interp, args_w[2])
         arg4 = unwrap(interp, args_w[3])
         arg5 = unwrap(interp, args_w[4])
-        return wrap(interp, python_space.call_method(wp_rcvr, methodname, arg1, arg2, arg3, arg4, arg5))
+        return wrap(interp, py_space.call_method(wp_rcvr, methodname, arg1, arg2, arg3, arg4, arg5))
     elif args_w_len == 6:
         arg1 = unwrap(interp, args_w[0])
         arg2 = unwrap(interp, args_w[1])
@@ -263,7 +311,7 @@ def _call_method(interp, wp_rcvr, methodname, args_w):
         arg4 = unwrap(interp, args_w[3])
         arg5 = unwrap(interp, args_w[4])
         arg6 = unwrap(interp, args_w[5])
-        return wrap(interp, python_space.call_method(wp_rcvr, methodname, arg1, arg2, arg3, arg4, arg5, arg6))
+        return wrap(interp, py_space.call_method(wp_rcvr, methodname, arg1, arg2, arg3, arg4, arg5, arg6))
     elif args_w_len == 7:
         arg1 = unwrap(interp, args_w[0])
         arg2 = unwrap(interp, args_w[1])
@@ -272,7 +320,7 @@ def _call_method(interp, wp_rcvr, methodname, args_w):
         arg5 = unwrap(interp, args_w[4])
         arg6 = unwrap(interp, args_w[5])
         arg7 = unwrap(interp, args_w[6])
-        return wrap(interp, python_space.call_method(wp_rcvr, methodname, arg1, arg2, arg3, arg4, arg5, arg6, arg7))
+        return wrap(interp, py_space.call_method(wp_rcvr, methodname, arg1, arg2, arg3, arg4, arg5, arg6, arg7))
     elif args_w_len == 8:
         arg1 = unwrap(interp, args_w[0])
         arg2 = unwrap(interp, args_w[1])
@@ -282,12 +330,13 @@ def _call_method(interp, wp_rcvr, methodname, args_w):
         arg6 = unwrap(interp, args_w[5])
         arg7 = unwrap(interp, args_w[6])
         arg8 = unwrap(interp, args_w[7])
-        return wrap(interp, python_space.call_method(wp_rcvr, methodname, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8))
-    return wrap(interp, python_space.call_method(wp_rcvr, methodname))
+        return wrap(interp, py_space.call_method(wp_rcvr, methodname, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8))
+    return wrap(interp, py_space.call_method(wp_rcvr, methodname))
 
 @PythonPlugin.expose_primitive(compiled_method=True)
 @jit.unroll_safe
 def send(interp, s_frame, argcount, w_method):
+    # import pdb; pdb.set_trace()
     args_w = s_frame.peek_n(argcount)
     w_literal2 = w_method.literalat0(interp.space, 2)
     methodname = ""
@@ -306,5 +355,5 @@ def send(interp, s_frame, argcount, w_method):
     try:
         return _call_method(interp, wp_rcvr, methodname, args_w)
     except OperationError as operationerr:
-        print operationerr.errorstr(python_space)
+        print operationerr.errorstr(py_space)
         raise PrimitiveFailedError
