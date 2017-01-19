0ac489cb7abe935fb99a27a124dd5adee330dc04
Fabio Niephaus <code@fniephaus.com>
2017-01-20 00:26:22 +0100

    Use tweaked travis_wait
    Builds are not producing enough output

--- .travis/build-osx.sh
+++ .travis/build-osx.sh
@@ -13,6 +13,49 @@ if [[ -n "$PLUGINS" ]]; then
   plugins_suffix="-${PLUGINS}"
 fi
 
+travis_wait() {
+  local timeout=20
+
+  local cmd="$@"
+
+  $cmd &
+  local cmd_pid=$!
+
+  travis_jigger $! $timeout $cmd &
+  local jigger_pid=$!
+  local result
+
+  {
+    wait $cmd_pid 2>/dev/null
+    result=$?
+    ps -p$jigger_pid &>/dev/null && kill $jigger_pid
+  }
+
+  echo -e "\nThe command $cmd exited with $result."
+  return $result
+}
+
+travis_jigger() {
+  # helper method for travis_wait()
+  local cmd_pid=$1
+  shift
+  local timeout=$1 # in minutes
+  shift
+  local count=0
+
+  # clear the line
+  echo -e "\n"
+
+  while [ $count -lt $timeout ]; do
+    count=$(($count + 1))
+    echo -ne "."
+    sleep 60
+  done
+
+  echo -e "\nTimeout (${timeout} minutes) reached. Terminating \"$@\"\n"
+  kill -9 $cmd_pid
+}
+
 case "$BUILD_ARCH" in
   32bit)
     python .build/build.py $plugins
@@ -22,7 +65,7 @@ case "$BUILD_ARCH" in
     # $EX rm -rf .build/pypy/rpython/_cache
     ;;
   64bit)
-    pypy .build/build.py -- $plugins
+    travis_wait pypy .build/build.py -- $plugins
     exitcode=$?
     cp rsqueak rsqueak-x86_64-${UNAME}$plugins_suffix-jit-$TRAVIS_COMMIT || true
     # python .build/jittests.py
