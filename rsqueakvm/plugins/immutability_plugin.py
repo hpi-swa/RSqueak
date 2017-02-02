8059e70eb7cbbbf0f294a387dd905c32abe2d357
Fabio Niephaus <code@fniephaus.com>
2017-02-02 11:48:57 +0100

    Add immutable W_PointersObject cls w/ fixed size

--- rsqueakvm/plugins/immutability_plugin.py
+++ rsqueakvm/plugins/immutability_plugin.py
@@ -1,9 +1,15 @@
 from rsqueakvm.error import PrimitiveFailedError
 from rsqueakvm.model import *
 from rsqueakvm.plugins.plugin import Plugin
-from rsqueakvm.storage_classes import BYTES, POINTERS, WORDS
+from rsqueakvm.storage_classes import BYTES, POINTERS, WORDS, ClassShadow
 
-from rpython.rlib import jit
+from rpython.rlib import jit, rerased
+from rpython.rlib.unroll import unrolling_iterable
+
+"""
+Documentation goes here.
+"""
+
 
 ImmutabilityPlugin = Plugin()
 
@@ -17,6 +23,7 @@ WRITE_OPERATIONS = [
     'setchar', 'short_atput0', 'setwords', 'convert_to_bytes_layout',
     'setbytes', 'mutate'
 ]
+STORAGE_ATTR_TEMPLATE = "storage_%d"
 
 
 def immutable_class(cls):
@@ -32,32 +39,85 @@ def immutable_class(cls):
     return cls
 
 
-def _add_immutable_w_pointersobject_subclass():
-    @immutable_class
-    class W_PointersObject_Immutable(W_PointersObject):
-        _attrs_ = ['storage', 'w_class', '_instsize']
-        _immutable_fields_ = ['storage[*]', '_instsize']
-        repr_classname = '%s_Immutable' % W_PointersObject.repr_classname
+@immutable_class
+class W_PointersObject_AbstractImmutable(W_PointersObject):
+    _attrs_ = []
+    _immutable_fields_ = []
+    repr_classname = ('%s_AbstractImmutable' %
+                      W_PointersObject.repr_classname)
+
+    def __init__(self, space, w_cls):
+        W_AbstractObjectWithIdentityHash.__init__(self)
+        # reuse strategy var to store cls
+        self.strategy = w_cls.as_class_get_shadow(space)
+
+    def getclass(self, space):
+        return self.strategy.w_self()
+
+    def class_shadow(self, space):
+        class_shadow = self.strategy
+        assert isinstance(class_shadow, ClassShadow)
+        return class_shadow
+
+    def size(self):
+        raise NotImplementedError
+
+    def fetch(self, space, n0):
+        raise NotImplementedError
+
+
+def _generate_fixed_w_pointersobject_class(n_storage):
+
+    storage_iter = unrolling_iterable(range(n_storage))
+    cls_name = '%s_Immutable_%s' % (W_PointersObject.repr_classname, n_storage)
+
+    class W_PointersObject_Immutable_Fixed(W_PointersObject_AbstractImmutable):
+        _storages_ = [(STORAGE_ATTR_TEMPLATE % x) for x in storage_iter]
+        _attrs_ = _storages_
+        _immutable_fields_ = _storages_
+        repr_classname = cls_name
 
         def __init__(self, space, w_cls, pointers_w):
-            W_AbstractObjectWithIdentityHash.__init__(self)
-            self.w_class = w_cls
-            self.storage = pointers_w
-            self._instsize = self.w_class.as_class_get_shadow(space).instsize()
-
-        def getclass(self, space):
-            return self.w_class
+            W_PointersObject_AbstractImmutable.__init__(self, space, w_cls)
+            for x in storage_iter:
+                setattr(self, STORAGE_ATTR_TEMPLATE % x, pointers_w[x])
 
         def size(self):
-            return len(self.storage)
-
-        def instsize(self):
-            return self._instsize
+            return n_storage
 
         def fetch(self, space, n0):
+            for x in storage_iter:
+                if x == n0:
+                    return getattr(self, STORAGE_ATTR_TEMPLATE % x)
+            raise IndexError
             return self.storage[n0]
 
-    W_PointersObject.immutable_subclass = W_PointersObject_Immutable
+    W_PointersObject_Immutable_Fixed.__name__ = cls_name
+
+    return W_PointersObject_Immutable_Fixed
+
+
+def _immutable_w_pointersobject_subclass():
+    class W_PointersObject_Immutable_N(W_PointersObject_AbstractImmutable):
+        _immutable_fields_ = ['_storage[*]']
+        repr_classname = '%s_Immutable_N' % W_PointersObject.repr_classname
+        erase, unerase = rerased.new_erasing_pair('storage_eraser')
+
+        def __init__(self, space, w_cls, pointers_w):
+            W_PointersObject_AbstractImmutable.__init__(self, space, w_cls)
+            self._storage = self.erase(pointers_w)
+
+        def size(self):
+            return len(self.unerase(self._storage))
+
+        def fetch(self, space, n0):
+            return self.unerase(self._storage)[n0]
+
+    classes = []
+    for n_storage in range(0, 10):
+        classes.append(_generate_fixed_w_pointersobject_class(n_storage))
+    classes.append(W_PointersObject_Immutable_N)
+    return classes
 
 
 def _add_immutable_w_bytesobject_subclass():
@@ -101,7 +161,20 @@ def _patch_w_object():
     W_Object.is_immutable = is_immutable
 
 _patch_w_object()
-_add_immutable_w_pointersobject_subclass()
+
+pointers_classes = _immutable_w_pointersobject_subclass()
+pointers_class_iter = unrolling_iterable(enumerate(pointers_classes))
+
+
+def select_pointers_class(storage):
+    length = len(storage)
+    for i, cls in pointers_class_iter:
+        if i == length:
+            return cls
+    # otherwise:
+    return pointers_classes[-1]
+
+
 _add_immutable_w_bytesobject_subclass()
 _add_immutable_w_wordsobject_subclass()
 
@@ -113,13 +186,15 @@ def primitiveImmutableFrom(interp, s_frame, w_cls, w_obj):
 
     if instance_kind == POINTERS:
         pointers = w_obj.fetch_all(space)
-        return W_PointersObject.immutable_subclass(space, w_cls, pointers)
+        cls = select_pointers_class(pointers)
+        return cls(space, w_cls, pointers)
     elif instance_kind == BYTES and isinstance(w_obj, W_BytesObject):
         return W_BytesObject.immutable_subclass(space, w_cls, w_obj.bytes)
     elif instance_kind == WORDS and isinstance(w_obj, W_WordsObject):
         return W_WordsObject.immutable_subclass(space, w_cls, w_obj.words)
 
     raise PrimitiveFailedError
+
 
 @ImmutabilityPlugin.expose_primitive(unwrap_spec=None)
 def primitiveImmutableFromArgs(interp, s_frame, argcount):
@@ -130,7 +205,8 @@ def primitiveImmutableFromArgs(interp, s_frame, argcount):
     instance_kind = w_cls.as_class_get_shadow(space).get_instance_kind()
 
     if instance_kind == POINTERS:
-        return W_PointersObject.immutable_subclass(space, w_cls, w_arguments)
+        cls = select_pointers_class(w_arguments)
+        return cls(space, w_cls, w_arguments)
     # TBD:
     # elif instance_kind == BYTES and isinstance(w_obj, W_BytesObject):
     #     return W_BytesObject.immutable_subclass(space, w_cls, w_obj.bytes)
