9f5b64d47f8971175c8dc6ef0a9fdd56bf8c2ac9
jchromik <jonas.chromik@student.hpi.de>
2017-02-09 13:52:46 +0100

    add test for primitiveImmutableFrom [ci skip]

--- rsqueakvm/test/test_immutability_plugin.py
+++ rsqueakvm/test/test_immutability_plugin.py
@@ -2,6 +2,7 @@ import py
 import pytest
 
 from rsqueakvm import storage_classes
+from rsqueakvm.error import PrimitiveFailedError
 from rsqueakvm.plugins.immutability.bytes import W_Immutable_BytesObject
 from rsqueakvm.plugins.immutability.pointers import (
     select_immutable_pointers_class)
@@ -74,7 +75,67 @@ def test_W_Immutable_WordsObject():
     py.test.raises(AssertionError, lambda: w_iwords.getword(20))
 
 def test_primIsImmutable():
+    w_class = bootstrap_class(0, format=storage_classes.WORDS)
+    w_words = w_class.as_class_get_shadow(space).new(20)
+    w_iwords = W_Immutable_WordsObject(space, w_class, w_words.words)
     assert external_call(space,
         'ImmutabilityPlugin',
         'primitiveIsImmutable',
-        [space.w("foo")]) == space.w_false
+        [w_words]) == space.w_false
+    assert external_call(space,
+        'ImmutabilityPlugin',
+        'primitiveIsImmutable',
+        [w_iwords]) == space.w_true
+
+def test_primImmutableFrom_bytes():
+    w_bytes_cls = bootstrap_class(0, format=storage_classes.BYTES)
+    w_bytes_obj = w_bytes_cls.as_class_get_shadow(space).new(20)
+    w_ibytes_obj = external_call(space,
+        'ImmutabilityPlugin',
+        'primitiveImmutableFrom',
+        [w_bytes_cls, w_bytes_obj])
+    assert w_ibytes_obj.is_immutable()
+    assert w_ibytes_obj.getclass(space).is_same_object(w_bytes_cls)
+    assert w_ibytes_obj.size() == 20
+    assert w_ibytes_obj.getchar(3) == '\x00'
+    w_ibytes_obj.setchar(3, '\xAA')
+    assert w_ibytes_obj.getchar(3) == '\x00'
+
+def test_primImmutableFrom_pointers():
+    size = 10
+    w_pointers_cls = bootstrap_class(0)
+    w_pointers_obj = W_PointersObject(space, w_pointers_cls, size)
+    w_pointers_obj.store(space, 0, space.w_true)
+    w_ipointers_obj = external_call(space,
+        'ImmutabilityPlugin',
+        'primitiveImmutableFrom',
+        [w_pointers_cls, w_pointers_obj])
+    assert w_ipointers_obj.is_immutable()
+    assert w_ipointers_obj.getclass(space).is_same_object(w_pointers_cls)
+    assert w_ipointers_obj.size() == size
+    assert w_ipointers_obj.fetch(space, 0) is space.w_true;
+    w_ipointers_obj.store(space, 0, space.w_false)
+    assert w_ipointers_obj.fetch(space, 0) is space.w_true;
+
+def test_primImmutableFrom_words():
+    w_words_cls = bootstrap_class(0, format=storage_classes.WORDS)
+    w_words_obj = w_words_cls.as_class_get_shadow(space).new(20)
+    w_iwords_obj = external_call(space,
+        'ImmutabilityPlugin',
+        'primitiveImmutableFrom',
+        [w_words_cls, w_words_obj])
+    assert w_iwords_obj.is_immutable()
+    assert w_iwords_obj.getclass(space).is_same_object(w_words_cls)
+    assert w_iwords_obj.size() == 20
+    assert w_iwords_obj.getword(3) == 0
+    w_iwords_obj.setword(3, 42)
+    assert w_iwords_obj.getword(3) == 0
+
+def test_primImmutableFrom_float():
+    w_float_cls = bootstrap_class(0, format=storage_classes.FLOAT)
+    w_float_obj = w_float_cls.as_class_get_shadow(space).new(20)
+    with py.test.raises(PrimitiveFailedError):
+        external_call(space,
+            'ImmutabilityPlugin',
+            'primitiveImmutableFrom',
+            [w_float_cls, w_float_obj])
