6f56f847ec4e475efcc65d329bf73151002b1578
Fabio Niephaus <code@fniephaus.com>
2017-02-09 22:51:26 +0100

    Improve exception handling
    Only catch exceptions that cannot be catched by any blocks in any frames.

--- rsqueakvm/plugins/python/patching.py
+++ rsqueakvm/plugins/python/patching.py
@@ -1,6 +1,11 @@
 from rsqueakvm.plugins.python import global_state as gs
 
 from pypy.module.pypyjit.interp_jit import PyFrame, PyPyJitDriver
+from pypy.interpreter.pyopcode import SApplicationException
+
+from pypy.tool.stdlib_opcode import bytecode_spec
+
+opcodedesc = bytecode_spec.opcodedesc
 
 old_init_frame = PyFrame.__init__
 old_execute_frame = PyFrame.execute_frame
@@ -27,14 +32,45 @@ def new_execute_frame(self, w_inputvalue=None, operr=None):
         return new_execute_frame(new_frame, w_inputvalue, operr)
 
 
+def get_exception(self, handlerposition):
+    "Retrieve exception to catch"
+    next_opcode = ord(self.pycode.co_code[handlerposition + 1])
+    if next_opcode == opcodedesc.LOAD_GLOBAL.index:
+        global_index = ord(self.pycode.co_code[handlerposition + 2])
+        return self._load_global(self.getname_u(global_index))
+    elif next_opcode == opcodedesc.LOAD_NAME.index:
+        if self.getorcreatedebug().w_locals is self.get_w_globals():
+            global_index = ord(self.pycode.co_code[handlerposition + 2])
+            return self._load_global(self.getname_u(global_index))
+    return None
+
+
+def has_exception_handler(self, operr):
+    "Returns True if this frame or one of his parents are able to handle operr"
+    frame = self
+    while frame is not None:
+        block = frame.lastblock
+        while block is not None:
+            if (block.handling_mask & SApplicationException.kind) != 0:
+                block_exc = get_exception(frame, block.handlerposition)
+                if block_exc is not None:
+                    if gs.py_space.exception_match(operr.w_type, block_exc):
+                        return True
+            block = block.previous
+        frame = frame.f_backref()
+    return False
+
+
 def new_handle_operation_error(self, ec, operr, attach_tb=True):
     if isinstance(operr, gs.RestartException):
         print 'Re-raising RestartException'
         raise operr
-    gs.wp_error.set(operr.get_w_value(gs.py_space))
-    print 'Python error caught'
-    # import pdb; pdb.set_trace()
-    gs.switch_action.perform()
+
+    if not self.has_exception_handler(operr):
+        # import pdb; pdb.set_trace()
+        gs.wp_error.set(operr.get_w_value(gs.py_space))
+        print 'Python error caught'
+        gs.switch_action.perform()
     return old_handle_operation_error(self, ec, operr, attach_tb)
 
 
@@ -49,4 +85,6 @@ def patch_pypy():
 
     PyFrame.__init__ = __init__frame
     PyFrame.execute_frame = new_execute_frame
+    PyFrame.get_exception = get_exception
+    PyFrame.has_exception_handler = has_exception_handler
     PyFrame.handle_operation_error = new_handle_operation_error
